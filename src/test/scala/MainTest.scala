451786f2ae9510f9fd724ccaf981cac1995b7d05
torstein-vik <torsteinv64@gmail.com>
2017-10-24 00:05:41 +0200

    Added Test for algebra syntax and semantics

--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -1,7 +1,31 @@
 import org.scalatest.FunSuite
 
+import io.zetatypes.general._
+
 class MainTest extends FunSuite {
-    test ("dummy test") {
-        assert(true)
+    test ("algebra test") {
+        class TestInteger (val x : Int) extends RingElement[TestInteger]{
+            override def +(that : TestInteger) = TestInteger(x + that.x) 
+            override def *(that : TestInteger) = TestInteger(x * that.x)
+            
+            def ==(that : TestInteger) = that.x == x
+            override def toString = "TestInteger " + x
+        }
+        
+        object TestInteger extends (Int => TestInteger){
+            override def apply (x : Int) = new TestInteger(x)
+        }
+        
+        object TestIntegers extends RingClass[TestInteger](TestInteger(0), TestInteger(1))
+        
+        assert(TestInteger(2) == TestInteger(2))
+        
+        assert(TestIntegers.additive      .identity == TestInteger(0))
+        assert(TestIntegers.multiplicative.identity == TestInteger(1))
+        assert(TestInteger(1) + TestInteger(2) == TestInteger(3))
+        assert(TestInteger(2) * TestInteger(3) == TestInteger(6))
+        
+        assert(TestIntegers.additive      .combine(TestInteger(1), TestInteger(2)) == TestInteger(3))
+        assert(TestIntegers.multiplicative.combine(TestInteger(2), TestInteger(3)) == TestInteger(6))
     }
 }
