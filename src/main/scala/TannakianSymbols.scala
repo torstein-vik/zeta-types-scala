fcd93bb8e1de0dee6a0696c3a29d9fad4972f441
torstein-vik <torsteinv64@gmail.com>
2017-10-25 02:21:32 +0200

    Added definition of Tannakian Symbol and some boilerplate

--- src/main/scala/TannakianSymbols.scala
+++ src/main/scala/TannakianSymbols.scala
@@ -5,7 +5,15 @@ import algebra.Rings.{Ring, RingClass, Element => RingElement}
 
 class TannakianSymbols[E <: MonoidElement, M <: Monoid[E]] extends RingClass[TannakianSymbol[E, M]](null, null)
 
-class TannakianSymbol[E <: MonoidElement, M <: Monoid[E]] () extends RingElement[TannakianSymbol[E, M]] {
-    override def +(that : TannakianSymbol[E, M]) = null
+class TannakianSymbol[E <: MonoidElement, M <: Monoid[E]] (elements : Array[Pair[E, Int]]) extends RingElement[TannakianSymbol[E, M]] {
+    override def +(that : TannakianSymbol[E, M]) = null 
     override def *(that : TannakianSymbol[E, M]) = null
+    
+    def ==(that : TannakianSymbol[E, M]) : Boolean = {
+        return false
+    }
+    
+    override def toString : String = {
+        return "string"
+    } 
 }
