ad5fd936bee4a148dbd7f10468aeab2b94a42121
torstein-vik <torsteinv64@gmail.com>
2017-10-27 17:33:32 +0200

    Added Tannakian symbol identity

--- src/main/scala/TannakianSymbols.scala
+++ src/main/scala/TannakianSymbols.scala
@@ -3,9 +3,13 @@ package io.zetatypes
 import algebra.Monoids.{Monoid, Element => MonoidElement}
 import algebra.Rings.{Ring, RingClass, Element => RingElement}
 
-class TannakianSymbols[E <: MonoidElement, M <: Monoid[E]] extends RingClass[TannakianSymbol[E, M]](null, null)
+class TannakianSymbols[E <: MonoidElement, M <: Monoid[E]](monoid : M) extends 
+    RingClass[TannakianSymbol[E, M]](
+    new TannakianSymbol(Seq.empty), 
+    new TannakianSymbol(Seq((monoid.identity, 1))))
 
-class TannakianSymbol[E <: MonoidElement, M <: Monoid[E]] (elements : Array[Pair[E, Int]]) extends RingElement[TannakianSymbol[E, M]] {
+class TannakianSymbol[E <: MonoidElement, M <: Monoid[E]] (elements : Seq[(E, Int)]) extends 
+    RingElement[TannakianSymbol[E, M]] {
     override def +(that : TannakianSymbol[E, M]) = null 
     override def *(that : TannakianSymbol[E, M]) = null
     
