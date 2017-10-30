f5ab52803442b08f8ba93933c38f51349eafddd0
Torstein Vik <torsteinv64@gmail.com>
2017-10-30 20:07:09 +0100

    Wrote test for TestInteger subtraction and inverse

--- src/test/scala/AlgebraTest.scala
+++ src/test/scala/AlgebraTest.scala
@@ -17,8 +17,11 @@ class AlgebraTest extends FunSuite {
         assert(TestIntegers.multiplicative.identity == TestInteger(1))
         assert(TestIntegers.zero == TestInteger(0))
         assert(TestIntegers.one  == TestInteger(1))
+        assert(TestIntegers.additive.inverse(4) == TestInteger(-4))
         assert(TestInteger(1) + TestInteger(2) == TestInteger(3))
         assert(TestInteger(2) * TestInteger(3) == TestInteger(6))
+        assert(- TestInteger(3) == TestInteger(-3))
+        assert(TestInteger(7) - TestInteger(4) == TestInteger(3))
         
         assert(TestIntegers.additive      .combine(TestInteger(1), TestInteger(2)) == TestInteger(3))
         assert(TestIntegers.multiplicative.combine(TestInteger(2), TestInteger(3)) == TestInteger(6))
