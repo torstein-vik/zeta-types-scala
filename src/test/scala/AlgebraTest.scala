71c5511aac092c9ce81808190138b7d0c84cdff8
Torstein Vik <torsteinv64@gmail.com>
2017-10-30 20:13:14 +0100

    Fixed typo in test

--- src/test/scala/AlgebraTest.scala
+++ src/test/scala/AlgebraTest.scala
@@ -17,7 +17,7 @@ class AlgebraTest extends FunSuite {
         assert(TestIntegers.multiplicative.identity == TestInteger(1))
         assert(TestIntegers.zero == TestInteger(0))
         assert(TestIntegers.one  == TestInteger(1))
-        assert(TestIntegers.additive.inverse(4) == TestInteger(-4))
+        assert(TestIntegers.additive.invert(4) == TestInteger(-4))
         assert(TestInteger(1) + TestInteger(2) == TestInteger(3))
         assert(TestInteger(2) * TestInteger(3) == TestInteger(6))
         assert(- TestInteger(3) == TestInteger(-3))
