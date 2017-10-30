7c9fbd03db7c4962dd49b87e7f9fda4d9dcf5bea
Torstein Vik <torsteinv64@gmail.com>
2017-10-30 23:49:36 +0100

    Renamed TestInteger to Integer

--- src/test/scala/AlgebraTest.scala
+++ src/test/scala/AlgebraTest.scala
@@ -1,69 +1,70 @@
 import org.scalatest.FunSuite
 
 import io.zetatypes.algebra._
+import io.zetatypes.algebra.structures._
 
 class AlgebraTest extends FunSuite {
     
     val (a, b, c) = (2, -3, 7)
     
     test ("equality test") {
-        assert(TestInteger(a) === TestInteger(a))
-        assert(TestInteger(b) === TestInteger(b))
-        assert(TestInteger(c) === TestInteger(c))
-        assert(TestInteger(c) !== TestInteger(c + 1))
-        assert(TestInteger(c) !== TestInteger(c - 1))
-        assert(TestInteger(c) !== TestInteger(c + 3))
+        assert(Integer(a) === Integer(a))
+        assert(Integer(b) === Integer(b))
+        assert(Integer(c) === Integer(c))
+        assert(Integer(c) !== Integer(c + 1))
+        assert(Integer(c) !== Integer(c - 1))
+        assert(Integer(c) !== Integer(c + 3))
     }
     
     test ("identities test") {
-        assert(TestIntegers.additive.identity === TestInteger(0))
-        assert(TestIntegers.multiplicative.identity === TestInteger(1))
+        assert(Integers.additive.identity === Integer(0))
+        assert(Integers.multiplicative.identity === Integer(1))
     }
     
     test ("zero and one test") {
-        assert(TestIntegers.zero === TestInteger(0))
-        assert(TestIntegers.one  === TestInteger(1))
+        assert(Integers.zero === Integer(0))
+        assert(Integers.one  === Integer(1))
     }
     
     test ("additive inverse test") {
-        assert(TestIntegers.additive.invert(TestInteger(a)) === TestInteger(-a))
-        assert(TestIntegers.additive.invert(TestInteger(b)) === TestInteger(-b))
-        assert(TestIntegers.additive.invert(TestInteger(c)) === TestInteger(-c))
+        assert(Integers.additive.invert(Integer(a)) === Integer(-a))
+        assert(Integers.additive.invert(Integer(b)) === Integer(-b))
+        assert(Integers.additive.invert(Integer(c)) === Integer(-c))
     }
     
     test ("addition test") {
-        assert(TestIntegers.additive.combine(TestInteger(a), TestInteger(b)) === TestInteger(a + b))
-        assert(TestIntegers.additive.combine(TestInteger(b), TestInteger(c)) === TestInteger(b + c))
-        assert(TestIntegers.additive.combine(TestInteger(c), TestInteger(a)) === TestInteger(c + a))
+        assert(Integers.additive.combine(Integer(a), Integer(b)) === Integer(a + b))
+        assert(Integers.additive.combine(Integer(b), Integer(c)) === Integer(b + c))
+        assert(Integers.additive.combine(Integer(c), Integer(a)) === Integer(c + a))
     }
     
     test ("multiplication test") {
-        assert(TestIntegers.multiplicative.combine(TestInteger(a), TestInteger(b)) === TestInteger(a * b))
-        assert(TestIntegers.multiplicative.combine(TestInteger(b), TestInteger(c)) === TestInteger(b * c))
-        assert(TestIntegers.multiplicative.combine(TestInteger(c), TestInteger(a)) === TestInteger(c * a))
+        assert(Integers.multiplicative.combine(Integer(a), Integer(b)) === Integer(a * b))
+        assert(Integers.multiplicative.combine(Integer(b), Integer(c)) === Integer(b * c))
+        assert(Integers.multiplicative.combine(Integer(c), Integer(a)) === Integer(c * a))
     }
     
     test ("notation adidition test") {
-        assert(TestInteger(a) + TestInteger(b) === TestInteger(a + b))
-        assert(TestInteger(b) + TestInteger(c) === TestInteger(b + c))
-        assert(TestInteger(c) + TestInteger(a) === TestInteger(c + a))
+        assert(Integer(a) + Integer(b) === Integer(a + b))
+        assert(Integer(b) + Integer(c) === Integer(b + c))
+        assert(Integer(c) + Integer(a) === Integer(c + a))
     }
     
     test ("notation multiplication test") {
-        assert(TestInteger(a) * TestInteger(b) === TestInteger(a * b))
-        assert(TestInteger(b) * TestInteger(c) === TestInteger(b * c))
-        assert(TestInteger(c) * TestInteger(a) === TestInteger(c * a))
+        assert(Integer(a) * Integer(b) === Integer(a * b))
+        assert(Integer(b) * Integer(c) === Integer(b * c))
+        assert(Integer(c) * Integer(a) === Integer(c * a))
     }
     
     test ("notation negation test") {
-        assert(-TestInteger(a) === TestInteger(-a))
-        assert(-TestInteger(b) === TestInteger(-b))
-        assert(-TestInteger(c) === TestInteger(-c))
+        assert(-Integer(a) === Integer(-a))
+        assert(-Integer(b) === Integer(-b))
+        assert(-Integer(c) === Integer(-c))
     }
     
     test ("subtraction test") {
-        assert(TestInteger(a) - TestInteger(b) === TestInteger(a - b))
-        assert(TestInteger(b) - TestInteger(c) === TestInteger(b - c))
-        assert(TestInteger(c) - TestInteger(a) === TestInteger(c - a))
+        assert(Integer(a) - Integer(b) === Integer(a - b))
+        assert(Integer(b) - Integer(c) === Integer(b - c))
+        assert(Integer(c) - Integer(a) === Integer(c - a))
     }
 }
