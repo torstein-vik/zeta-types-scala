5403139999b6e8c361f29f3abbc1725fb5a96142
Torstein Vik <torsteinv64@gmail.com>
2017-10-30 19:49:01 +0100

    Added Additive and Multiplicative Group

--- src/main/scala/algebra/Groups.scala
+++ src/main/scala/algebra/Groups.scala
@@ -11,3 +11,6 @@ trait Group[T <: GroupElement] extends PartialGroup[T] {
     def invert (x : T) : T
     def partialinvert (x : T) = Some(invert(x))
 }
+
+trait AdditiveGroup[T <: GroupElement with Additive[T]] extends AdditiveMonoid[T]
+trait MultiplicativeGroup[T <: GroupElement with Multiplicative[T]] extends MultiplicativeMonoid[T]
