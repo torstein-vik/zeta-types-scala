3463abf1b7c9eb4ba008ad063a8bdc670d24f818
Torstein Vik <torsteinv64@gmail.com>
2017-10-30 23:35:43 +0100

    Added some compilation assertions to TannakianSymbolTest

--- src/test/scala/TannakianSymbolTest.scala
+++ src/test/scala/TannakianSymbolTest.scala
@@ -1,5 +1,11 @@
 import org.scalatest.FunSuite
 
 class TannakianSymbolTest extends FunSuite{
-    
+    import io.zetatypes.tannakiansymbols.DSL._
+        
+    test ("Defining TannakianSymbol using multisets"){
+        assertCompiles("ms(1, 1, 2) / Ø")
+        assertCompiles("Ø / Ø")
+        assertCompiles("ms(1, 2, 3) / ms(1, 2, 3, -4)")
+    }
 }
