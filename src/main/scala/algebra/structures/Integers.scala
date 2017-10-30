d8eff9f82303bee444ec1a5c4c820401a66d8106
Torstein Vik <torsteinv64@gmail.com>
2017-10-31 00:12:55 +0100

    Redid Integer toString to Int toString

--- src/main/scala/algebra/structures/Integers.scala
+++ src/main/scala/algebra/structures/Integers.scala
@@ -6,6 +6,8 @@ case class Integer (x : Int) extends RingElement[Integer]{
     override def +(that : Integer) = Integer(x + that.x) 
     override def *(that : Integer) = Integer(x * that.x)
     override def negation() = Integer(-x)
+    
+    override def toString() = x.toString()
 }
 
 object Integers extends RingClass[Integer](Integer(0), Integer(1))
