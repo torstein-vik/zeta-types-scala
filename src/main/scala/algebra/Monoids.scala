f8f59f1997dadddf03a2706603bd3a8fa7a36e16
Torstein Vik <torsteinv64@gmail.com>
2017-10-30 19:45:31 +0100

    Enforced MonoidElement on Additive/Multiplicative Mondoid

--- src/main/scala/algebra/Monoids.scala
+++ src/main/scala/algebra/Monoids.scala
@@ -4,12 +4,12 @@ trait Monoid[T <: MonoidElement] extends Magma[T]{
     def identity : T
 }
 
-trait AdditiveMonoid[T <: Additive[T]] extends Monoid[T] {
+trait AdditiveMonoid[T <: MonoidElement with Additive[T]] extends Monoid[T] {
     def zero : T = identity
     override def combine (x : T, y : T) : T = x + y
 }
 
-trait MultiplicativeMonoid[T <: Multiplicative[T]] extends Monoid[T] {
+trait MultiplicativeMonoid[T <: MonoidElement with Multiplicative[T]] extends Monoid[T] {
     def one : T = identity
     override def combine (x : T, y : T) : T = x * y
 }
