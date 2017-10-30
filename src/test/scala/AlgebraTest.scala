9d17d8c6d43b43fd65f0016d84fe27ede1a13492
Torstein Vik <torsteinv64@gmail.com>
2017-10-30 23:15:05 +0100

    Split assertions into many tests in AlgebraTest

--- src/test/scala/AlgebraTest.scala
+++ src/test/scala/AlgebraTest.scala
@@ -11,20 +11,45 @@ class AlgebraTest extends FunSuite {
     
     object TestIntegers extends RingClass[TestInteger](TestInteger(0), TestInteger(1))
     
-    test ("ring test") {
+    test ("equality test") {
         assert(TestInteger(2) === TestInteger(2))
-        
-        assert(TestIntegers.additive      .identity === TestInteger(0))
+    }
+    
+    test ("identities test") {
+        assert(TestIntegers.additive.identity === TestInteger(0))
         assert(TestIntegers.multiplicative.identity === TestInteger(1))
+    }
+    
+    test ("zero and one test") {
         assert(TestIntegers.zero === TestInteger(0))
         assert(TestIntegers.one  === TestInteger(1))
+    }
+    
+    test ("additive inverse test") {
         assert(TestIntegers.additive.invert(TestInteger(4)) === TestInteger(-4))
-        assert(TestInteger(1) + TestInteger(2) === TestInteger(3))
-        assert(TestInteger(2) * TestInteger(3) === TestInteger(6))
-        assert(- TestInteger(3) === TestInteger(-3))
-        assert(TestInteger(7) - TestInteger(4) === TestInteger(3))
-        
-        assert(TestIntegers.additive      .combine(TestInteger(1), TestInteger(2)) === TestInteger(3))
+    }
+    
+    test ("addition test") {
+        assert(TestIntegers.additive.combine(TestInteger(1), TestInteger(2)) === TestInteger(3))
+    }
+    
+    test ("multiplication test") {
         assert(TestIntegers.multiplicative.combine(TestInteger(2), TestInteger(3)) === TestInteger(6))
+    }
+    
+    test ("notation adidition test") {
+        assert(TestInteger(1) + TestInteger(2) === TestInteger(3))
+    }
+    
+    test ("notation multiplication test") {
+        assert(TestInteger(2) * TestInteger(3) === TestInteger(6))    
+    }
+    
+    test ("notation negation test") {
+        assert(- TestInteger(3) === TestInteger(-3))
+    }
+    
+    test ("subtraction test") {
+        assert(TestInteger(7) - TestInteger(4) === TestInteger(3))    
     }
 }
