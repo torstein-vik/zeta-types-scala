10da30c641987a3539feb27c9002a8682912d7e8
Torstein Vik <torsteinv64@gmail.com>
2017-10-31 22:48:27 +0100

    Implemented multiset toString

--- src/main/scala/tannakiansymbols/Multiset.scala
+++ src/main/scala/tannakiansymbols/Multiset.scala
@@ -2,4 +2,12 @@ package io.zetatypes.tannakiansymbols
 
 class Multiset[T](val elements : T*) {
     
+    override def toString : String = {
+        if (elements.length == 0) {
+            return "Ø"
+        } else {
+            return elements.mkString("{", ", ", "}")
+        }
+    }
+    
 }
