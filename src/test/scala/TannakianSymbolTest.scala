b3225c6a945bde168e789e1e3b7e76c392fec03c
Torstein Vik <torsteinv64@gmail.com>
2017-10-31 22:17:09 +0100

    Added multiset toString test

--- src/test/scala/TannakianSymbolTest.scala
+++ src/test/scala/TannakianSymbolTest.scala
@@ -120,5 +120,13 @@ class MultisetTest extends FunSuite {
             assert(ms(4, 8, 8, 2, 9) === ms(2, 4, 8, 8, 9))
         }
         
+        test ("Multiset toString test") {
+            assert(Ø[Int].toString === "Ø")
+            
+            assert(ms(1, 2, 3).toString === "{1, 2, 3}")
+            assert(ms(1, 2, 2, 3).toString === "{1, 2, 2, 3}")
+            assert(ms(10).toString === "{10}")
+        }
+        
     }
 }
