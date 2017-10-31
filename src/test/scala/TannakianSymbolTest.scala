3e8f8e6fc1542a4c9c2cc5b025caed61b225387a
Torstein Vik <torsteinv64@gmail.com>
2017-10-31 22:07:10 +0100

    Added multiset equality test

--- src/test/scala/TannakianSymbolTest.scala
+++ src/test/scala/TannakianSymbolTest.scala
@@ -103,7 +103,21 @@ class MultisetTest extends FunSuite {
     {// With DSL
         import io.zetatypes.tannakiansymbols.DSL._
         
-        
+        test ("Multiset equality") {
+            assert(Ø === ms())
+            
+            assert(ms(1, 2) === ms(1, 2))
+            assert(ms(1, 2, 2, 3) === ms(1, 2, 2, 3))
+            
+            assert(ms(1, 2, 3) !== ms(1, 2))
+            assert(ms(1, 2, 3) !== ms(1, 2, 3, 4))
+            assert(ms(1, 2, 3) !== ms(1, 2, 2, 3))
+            assert(ms(2, 3) !== ms(2, 2))
+            
+            assert(ms(1, 2) === ms(2, 1))
+            assert(ms(1, 2, 2) === ms(2, 1, 2))
+            assert(ms(4, 8, 8, 2, 9) === ms(2, 4, 8, 8, 9))
+        }
         
     }
 }
