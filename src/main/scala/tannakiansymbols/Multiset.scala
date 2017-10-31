1ecfae294746bb894631b1d572cca42b7da715f2
Torstein Vik <torsteinv64@gmail.com>
2017-11-01 00:57:08 +0100

    Added method / for creating tannakian symbols from multisets

--- src/main/scala/tannakiansymbols/Multiset.scala
+++ src/main/scala/tannakiansymbols/Multiset.scala
@@ -28,4 +28,10 @@ class Multiset[T](val elements : T*) {
         return el
     }
     
+    def /[S <: T with MonoidElement](downstairs : Multiset[T])(implicit ev: T <:< S) : TannakianSymbol[S] = {
+        var el : Map[S, Int] = Map()
+                   elements.foreach(x => el += ev(x) -> (el.getOrElse(ev(x), 0) + 1))
+        downstairs.elements.foreach(x => el += ev(x) -> (el.getOrElse(ev(x), 0) - 1))
+        return new TannakianSymbol[S](el.toSeq)
+    }
 }
