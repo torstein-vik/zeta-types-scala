799acca1ff8c1f0d03607ac2fa02978a970e08dd
Torstein Vik <torsteinv64@gmail.com>
2017-11-01 20:41:54 +0100

    Implemented integral psi

--- src/main/scala/algebra/structures/Integers.scala
+++ src/main/scala/algebra/structures/Integers.scala
@@ -2,12 +2,14 @@ package io.zetatypes.algebra.structures
 
 import io.zetatypes.algebra._
 
-case class Integer (x : Int) extends RingElement[Integer]{
+case class Integer (x : Int) extends StandardLambdaRingElement[Integer]{
     override def +(that : Integer) = Integer(x + that.x) 
     override def *(that : Integer) = Integer(x * that.x)
     override def negation() = Integer(-x)
     
+    override def psi(n : Int) = this
+    
     override def toString() = x.toString()
 }
 
-object Integers extends RingClass[Integer](Integer(0), Integer(1))
+object Integers extends RingClass[Integer](Integer(0), Integer(1)) with StandardLambdaRing[Integer]
