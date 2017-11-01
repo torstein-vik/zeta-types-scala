f7ac6858c5d9e77cbcbfb83db1c80d31801bdf7f
Torstein Vik <torsteinv64@gmail.com>
2017-11-01 22:07:18 +0100

    Added ^ for Integer type

--- src/main/scala/algebra/Magma.scala
+++ src/main/scala/algebra/Magma.scala
@@ -2,6 +2,8 @@ package io.zetatypes.algebra
 
 trait AlgebraicElement
 
+import scala.annotation.tailrec
+
 class AlgebraicException(msg : String) extends Exception(msg)
 
 trait Magma[T <: AlgebraicElement] {
@@ -14,4 +16,14 @@ trait Additive[that <: Additive[that]] extends AlgebraicElement{
 
 trait Multiplicative[that <: Multiplicative[that]] extends AlgebraicElement {
     def *(y : that) : that
+    def ^(n : Int)(implicit ev: this.type <:< that) : that = n match {
+        case n if n > 0 => expTailRec(n, ev(this))
+        case _ => throw new AlgebraicException("Magmatic expontiation requires n > 0")
+    }
+    
+    @tailrec
+    private def expTailRec(n : Int, acc : that) : that = {
+        if (n <= 1) return acc
+        return expTailRec(n - 1, this * acc)
+    } 
 }
