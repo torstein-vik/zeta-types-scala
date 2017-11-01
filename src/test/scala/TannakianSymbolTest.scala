6b72acacb4f8143115b38e481749a2fd40e1a773
Torstein Vik <torsteinv64@gmail.com>
2017-11-01 18:34:20 +0100

    Added TS addition test

--- src/test/scala/TannakianSymbolTest.scala
+++ src/test/scala/TannakianSymbolTest.scala
@@ -112,6 +112,16 @@ class TannakianSymbolTest extends FunSuite{
             Seq(X, Y, Z, W).foreach(x => assert(x === x.upstairs / x.downstairs))
         }
     }
+    
+    {// Addition test
+        import io.zetatypes.tannakiansymbols.DSL._
+        import io.zetatypes.algebra.structures.DSL._
+        
+        assert(ms(1, 2) / Ø + ms(1, 2) / Ø === ms(1, 1, 2, 2) / Ø)
+        assert(ms(1, 2) / Ø + Ø / ms(1, 2) === Ø / Ø)
+        assert(ms(1) / Ø + Ø / ms(2) === ms(1) / ms(2))
+        assert(ms(1, 2, 2, 2) / ms(3, 3) + ms(3, 4, 5) / ms(2, 7) === ms(1, 2, 2, 4, 5) / ms(3, 7))
+    }
 }
 
 class MultisetTest extends FunSuite {
