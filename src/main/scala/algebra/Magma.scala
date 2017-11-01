b0ca5ade60626fdac03e288e935930ca813fe68e
Torstein Vik <torsteinv64@gmail.com>
2017-11-01 22:39:35 +0100

    Added repeated magmatic combination to Magma trait as well

--- src/main/scala/algebra/Magma.scala
+++ src/main/scala/algebra/Magma.scala
@@ -8,6 +8,16 @@ class AlgebraicException(msg : String) extends Exception(msg)
 
 trait Magma[T <: AlgebraicElement] {
     def combine (x : T, y : T) : T
+    def repeated (x : T, n : Int) : T = n match {
+        case n if n > 0 => expTailRec(n, x, x)
+        case _ => throw new AlgebraicException("Magmatic repeated addition requires n > 0")
+    }
+    
+    @tailrec
+    private def expTailRec(n : Int, x : T, acc : T) : T = {
+        if (n <= 1) return acc
+        return expTailRec(n - 1, x, combine(x, acc))
+    }  
 }
 
 trait Additive[that <: Additive[that]] extends AlgebraicElement{
