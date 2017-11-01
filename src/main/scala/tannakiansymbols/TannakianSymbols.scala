c1784dab2ea0dc83de791e92b47b7c07448a2376
Torstein Vik <torsteinv64@gmail.com>
2017-11-01 22:40:15 +0100

    Made TannakianSymbol and TS into StandardLambdaRing

--- src/main/scala/tannakiansymbols/TannakianSymbols.scala
+++ src/main/scala/tannakiansymbols/TannakianSymbols.scala
@@ -6,9 +6,10 @@ case class TS[E <: MonoidElement](monoid : Monoid[E]) extends
     RingClass[TannakianSymbol[E]](
     new TannakianSymbol(Seq.empty)(monoid), 
     new TannakianSymbol(Seq((monoid.identity, 1)))(monoid))
+    with StandardLambdaRing[TannakianSymbol[E]]
 
 class TannakianSymbol[E <: MonoidElement] (val elements : Seq[(E, Int)])(implicit monoid : Monoid[E]) extends 
-    RingElement[TannakianSymbol[E]] {
+    RingElement[TannakianSymbol[E]] with StandardLambdaRingElement[TannakianSymbol[E]]{
     override def +(that : TannakianSymbol[E]) = new TannakianSymbol(elements ++ that.elements).cleanup 
     
     override def *(that : TannakianSymbol[E]) = new TannakianSymbol(for ((x, i) <- elements; (y, j) <- that.elements) yield (monoid.combine(x, y), i * j)).cleanup
