ca4c4276ca363fdb3e0af5e7b6bb97cde6c71084
Torstein Vik <torsteinv64@gmail.com>
2017-11-01 22:19:03 +0100

    Added ++, repeated magmatic addition

--- src/main/scala/algebra/Magma.scala
+++ src/main/scala/algebra/Magma.scala
@@ -12,6 +12,16 @@ trait Magma[T <: AlgebraicElement] {
 
 trait Additive[that <: Additive[that]] extends AlgebraicElement{
     def +(y : that) : that
+    def ++(n : Int)(implicit ev: this.type <:< that) : that = n match {
+        case n if n > 0 => expTailRec(n, ev(this))
+        case _ => throw new AlgebraicException("Magmatic repeated addition requires n > 0")
+    }
+    
+    @tailrec
+    private def expTailRec(n : Int, acc : that) : that = {
+        if (n <= 1) return acc
+        return expTailRec(n - 1, this + acc)
+    } 
 }
 
 trait Multiplicative[that <: Multiplicative[that]] extends AlgebraicElement {
