395eab00fff9fd406d900910127296f3efeb8ec2
Torstein Vik <torsteinv64@gmail.com>
2017-11-01 19:03:04 +0100

    Added implicit monoid to TannakianSymbol class

--- src/main/scala/tannakiansymbols/TannakianSymbols.scala
+++ src/main/scala/tannakiansymbols/TannakianSymbols.scala
@@ -4,13 +4,15 @@ import io.zetatypes.algebra._
 
 case class TS[E <: MonoidElement](monoid : Monoid[E]) extends 
     RingClass[TannakianSymbol[E]](
-    new TannakianSymbol(Seq.empty), 
-    new TannakianSymbol(Seq((monoid.identity, 1))))
+    new TannakianSymbol(Seq.empty)(monoid), 
+    new TannakianSymbol(Seq((monoid.identity, 1)))(monoid))
 
-class TannakianSymbol[E <: MonoidElement] (val elements : Seq[(E, Int)]) extends 
+class TannakianSymbol[E <: MonoidElement] (val elements : Seq[(E, Int)])(implicit monoid : Monoid[E]) extends 
     RingElement[TannakianSymbol[E]] {
     override def +(that : TannakianSymbol[E]) = new TannakianSymbol(elements ++ that.elements).cleanup 
-    override def *(that : TannakianSymbol[E]) = null
+    
+    override def *(that : TannakianSymbol[E]) = new TannakianSymbol(for ((x, i) <- elements; (y, j) <- that.elements) yield (monoid.combine(x, y), i * j)).cleanup
+    
     override def negation() = new TannakianSymbol(elements.map({case (x, i) => (x, -i)}))
     
     override def equals(that : Any) : Boolean = {
