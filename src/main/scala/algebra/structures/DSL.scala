498b840c32b8418bc10c5ba473864260d9038a4e
Torstein Vik <torsteinv64@gmail.com>
2017-11-01 18:27:47 +0100

    Added implicit functor Ring => Monoid using multiplicative structure

--- src/main/scala/algebra/structures/DSL.scala
+++ src/main/scala/algebra/structures/DSL.scala
@@ -2,6 +2,10 @@ package io.zetatypes.algebra.structures
 
 import scala.language.implicitConversions
 
+import io.zetatypes.algebra._
+
 package object DSL {
     implicit def toInteger (x : Int) : Integer = Integer(x)
+    
+    implicit def multiplicativeMonoid[E <: RingElement[E]] (ring : Ring[E]) : Monoid[E] = ring.multiplicative
 }
