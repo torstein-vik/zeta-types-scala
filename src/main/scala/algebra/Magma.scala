a7e84d667e9cfaec29017976c1dce439dbcb35a4
Torstein Vik <torsteinv64@gmail.com>
2017-11-01 21:41:31 +0100

    Removed times, using Element-centric approach instead

--- src/main/scala/algebra/Magma.scala
+++ src/main/scala/algebra/Magma.scala
@@ -6,10 +6,6 @@ class AlgebraicException(msg : String) extends Exception(msg)
 
 trait Magma[T <: AlgebraicElement] {
     def combine (x : T, y : T) : T
-    def times (x : T)(n : Int) = n match {
-        case n if n > 0 => Seq.fill(n)(x).reduceLeft(combine(_, _))
-        case _ => throw new AlgebraicException("Magma times, n must be at least 1!")
-    }    
 }
 
 trait Additive[that <: Additive[that]] extends AlgebraicElement{
