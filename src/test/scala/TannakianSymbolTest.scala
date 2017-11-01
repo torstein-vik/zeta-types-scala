3dc6a7364cd1f60949ea0a5732b741f7d0137bc4
Torstein Vik <torsteinv64@gmail.com>
2017-11-01 18:06:21 +0100

    Added algebra structures DSL to ts test

--- src/test/scala/TannakianSymbolTest.scala
+++ src/test/scala/TannakianSymbolTest.scala
@@ -2,6 +2,7 @@ import org.scalatest.FunSuite
 
 class TannakianSymbolTest extends FunSuite{
     import scala.language.implicitConversions
+    import io.zetatypes.algebra.structures.DSL._
     import io.zetatypes.tannakiansymbols.DSL._
         
     test ("Defining TannakianSymbol using multisets"){
