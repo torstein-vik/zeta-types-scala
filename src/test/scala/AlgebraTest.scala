328dc9400fad04f84bf7aade032a33a235481072
Torstein Vik <torsteinv64@gmail.com>
2017-11-02 14:18:14 +0100

    Added rational ++ test

--- src/test/scala/AlgebraTest.scala
+++ src/test/scala/AlgebraTest.scala
@@ -270,4 +270,28 @@ class RationalTest extends FunSuite {
     }
     
     
+    test ("Integer module multiplication") {
+        assert((Rational(a, b) ++ 1) === Rational(a, b))
+        assert((Rational(b, c) ++ 1) === Rational(b, c))
+        assert((Rational(c, d) ++ 1) === Rational(c, d))
+        assert((Rational(d, a) ++ 1) === Rational(d, a))
+        assert((Rational(a, b) ++ 2) === Rational(a + a, b))
+        assert((Rational(b, c) ++ 2) === Rational(b + b, c))
+        assert((Rational(c, d) ++ 2) === Rational(c + c, d))
+        assert((Rational(d, a) ++ 2) === Rational(d + d, a))
+        assert((Rational(a, b) ++ 3) === Rational(a + a + a, b))
+        assert((Rational(b, c) ++ 3) === Rational(b + b + b, c))
+        assert((Rational(c, d) ++ 3) === Rational(c + c + c, d))
+        assert((Rational(d, a) ++ 3) === Rational(d + d + d, a))
+        
+        assertThrows[AlgebraicException] {
+            Rational(1, 2) ++ 0
+        }
+        
+        assertThrows[AlgebraicException] {
+            Rational(1, 2) ++ (-1)
+        }
+    }
+    
+    
 }
