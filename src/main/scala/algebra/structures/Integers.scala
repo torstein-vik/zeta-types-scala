ca9d2badf9b78e6016174d2007b8b5ebb2724287
Torstein Vik <torsteinv64@gmail.com>
2017-11-02 18:39:06 +0100

    Implemented Integer PartialQMult

--- src/main/scala/algebra/structures/Integers.scala
+++ src/main/scala/algebra/structures/Integers.scala
@@ -2,16 +2,25 @@ package org.zetatypes.algebra.structures
 
 import org.zetatypes.algebra._
 
-case class Integer (x : Int) extends StandardLambdaRingElement[Integer]{
+case class Integer (x : Int) extends StandardLambdaRingElement[Integer] with PartialQAlgebraElement[Integer]{
     override def +(that : Integer) = Integer(x + that.x) 
     override def *(that : Integer) = Integer(x * that.x)
     override def negation() = Integer(-x)
     
     override def psi(n : Int) = this
     
+    override def partialQMult(n : Rational) : Option[Integer] = {
+        val Rational(Integer(num), Integer(den)) = n
+        if ((num * x) % den == 0) {
+            return Some(Integer((num * x) / den))
+        } else {
+            return None
+        }
+    }
+    
     override def toString() = x.toString()
     
     override val canonicalRing = Integers
 }
 
-object Integers extends RingClass[Integer](Integer(0), Integer(1)) with StandardLambdaRing[Integer]
+object Integers extends RingClass[Integer](Integer(0), Integer(1)) with StandardLambdaRing[Integer] with PartialQAlgebra[Integer]
