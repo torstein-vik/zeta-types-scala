e2ccff6ca4b65d9f47c0d48dabe33d66e0e39fa8
Torstein Vik <torsteinv64@gmail.com>
2017-11-02 13:54:16 +0100

    Redid definition of Fractions

--- src/main/scala/algebra/structures/Fraction.scala
+++ src/main/scala/algebra/structures/Fraction.scala
@@ -2,9 +2,11 @@ package org.zetatypes.algebra.structures
 
 import org.zetatypes.algebra._
 
-case class Fractions[T] (ring : Ring[T]) extends RingClass[Fraction[T]](Fraction(ring.zero, ring.one), Fraction(ring.one, ring.one)) {
+case class Fractions[T <: RingElement[T]] (ring : Ring[T]) extends 
+    RingClass[Fraction[T]](
+    new Fraction(ring)(ring.zero, ring.one), 
+    new Fraction(ring)(ring.one,  ring.one))
     
-}
 
 case class Fraction[T] (numerator : T, denominator : T) extends RingElement[Fraction[T]]{
     
