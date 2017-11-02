4163aa1e4a432495a52caa33910a5be98153d4da
Torstein Vik <torsteinv64@gmail.com>
2017-11-02 17:52:09 +0100

    PartialQAlgebra semantics made element-centric

--- src/main/scala/algebra/QAlgebra.scala
+++ src/main/scala/algebra/QAlgebra.scala
@@ -2,8 +2,10 @@ package org.zetatypes.algebra
 
 import structures.Rational
 
-trait PartialQAlgebraElement[T <: PartialQAlgebraElement[T]] extends RingElement[T]
+trait PartialQAlgebraElement[T <: PartialQAlgebraElement[T]] extends RingElement[T]{
+    def partialQMult (n : Rational) : Option[T]
+}
 
 trait PartialQAlgebra[T <: PartialQAlgebraElement[T]] extends Ring[T] {
-    def partialQAlgebraOp (x : T)(n : Rational) : Option[T]
+    def partialQMult (x : T)(n : Rational) : Option[T] = x.partialQMult(n)
 }
