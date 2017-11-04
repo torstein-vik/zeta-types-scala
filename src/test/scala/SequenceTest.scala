bd0fb9c600b1cb7ccaf68994d8f872ed9fb187f2
Torstein Vik <torsteinv64@gmail.com>
2017-11-04 23:51:14 +0100

    Added env. for CachedSequence test

--- src/test/scala/SequenceTest.scala
+++ src/test/scala/SequenceTest.scala
@@ -3,4 +3,8 @@ import org.scalatest.FunSuite
 import org.zetatypes.sequences._
 
 class SequenceFactoryTest extends FunSuite {
+    
+    {// CachedSequence test
+        import org.zetatypes.sequences.DSL._
+    }
 }
