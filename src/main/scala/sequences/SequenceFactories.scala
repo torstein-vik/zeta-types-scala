6670992c6cabe6e626ce71edb79ae7ffe34d98ed
Torstein Vik <torsteinv64@gmail.com>
2017-11-04 23:49:47 +0100

    Added ShortSequence, factory based on some Seq

--- src/main/scala/sequences/SequenceFactories.scala
+++ src/main/scala/sequences/SequenceFactories.scala
@@ -6,6 +6,13 @@ trait SequenceFactory[E] {
     def length : Option[Int] = None
 }
 
+class ShortSequence[E] (seq : E*) extends SequenceFactory[E] {
+    def apply (seq : Sequence[E])(index : Int) = seq(index)
+    override def hasInternalCache = true
+    
+    override def length = Some(seq.length)
+}
+
 class ConstantSequence[E] (value : E) extends SequenceFactory[E] {
     def apply (seq : Sequence[E])(index : Int) = value
     override def hasInternalCache = true
