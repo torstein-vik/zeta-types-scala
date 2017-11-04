02a6e36b40a001509ee9efb1a1e987eaea7a4f17
Torstein Vik <torsteinv64@gmail.com>
2017-11-04 23:46:46 +0100

    Added sequence trait

--- src/main/scala/sequences/Sequences.scala
+++ src/main/scala/sequences/Sequences.scala
@@ -4,3 +4,8 @@ import scala.collection.immutable.IntMap
 
 class SequenceException(msg : String) extends Exception(msg)
 
+trait Sequence[E] {
+    def apply (index : Int) : E 
+    def length : Option[Int]
+    
+}
