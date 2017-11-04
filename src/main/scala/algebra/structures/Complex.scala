24e3b1ddf70ae876630c147870ed7ef6f97cbd5a
Torstein Vik <torsteinv64@gmail.com>
2017-11-04 14:22:47 +0100

    Added ComplexNumbers and ComplexNumber, for general ring

--- src/main/scala/algebra/structures/Complex.scala
+++ src/main/scala/algebra/structures/Complex.scala
@@ -1,2 +1,13 @@
 package org.zetatypes.algebra.structures
 
+import org.zetatypes.algebra._
+
+case class ComplexNumbers[T <: RingElement[T]] (ring : Ring[T]) extends 
+    RingClass[ComplexNumber[T]](
+    new ComplexNumber(ring)(ring.zero, ring.one), 
+    new ComplexNumber(ring)(ring.one,  ring.one))
+
+class ComplexNumber[T <: RingElement[T]] (val ring : Ring[T])(val real : T, val imaginary : T = ring.zero) extends RingElement[ComplexNumber[T]]{
+    
+    
+}
