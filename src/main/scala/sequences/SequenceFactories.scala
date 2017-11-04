3823f560ea4fc790fe2ec32bbeb897fcf6b0f54b
Torstein Vik <torsteinv64@gmail.com>
2017-11-04 23:48:59 +0100

    Added SequenceFactory trait, with apply, length, and hasInternalCache

--- src/main/scala/sequences/SequenceFactories.scala
+++ src/main/scala/sequences/SequenceFactories.scala
@@ -1,2 +1,8 @@
 package org.zetatypes.sequences
 
+trait SequenceFactory[E] {
+    def apply (seq : Sequence[E])(index : Int) : E
+    def hasInternalCache : Boolean = false
+    def length : Option[Int] = None
+}
+
