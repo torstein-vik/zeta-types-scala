2a6e0e2b680628a027196f0c2f51f67002395d99
Torstein Vik <torsteinv64@gmail.com>
2017-11-04 15:14:47 +0100

    Made Fraction ring implcit, removed optional denominator, now at subclass

--- src/main/scala/algebra/structures/Fraction.scala
+++ src/main/scala/algebra/structures/Fraction.scala
@@ -4,21 +4,22 @@ import org.zetatypes.algebra._
 
 case class Fractions[T <: RingElement[T]] (ring : Ring[T]) extends 
     RingClass[Fraction[T]](
-    new Fraction(ring)(ring.zero, ring.one), 
-    new Fraction(ring)(ring.one,  ring.one))
+    new Fraction(ring.zero, ring.one)(ring), 
+    new Fraction(ring.one,  ring.one)(ring))
 
-class Fraction[T <: RingElement[T]] (val ring : Ring[T])(val numerator : T, val denominator : T = ring.one) extends RingElement[Fraction[T]]{
+// TODO: Make ring implicit and remove default value, let subclasses deal with that
+class Fraction[T <: RingElement[T]] (val numerator : T, val denominator : T)(implicit val ring : Ring[T]) extends RingElement[Fraction[T]]{
     if (denominator == ring.zero) {
         throw new AlgebraicException("Denominator may not be zero!")
     }
     
     override lazy val canonicalRing = Fractions(ring)
     
-    override def +(that : Fraction[T]) = new Fraction(ring)(numerator * that.denominator + that.numerator * denominator, denominator * that.denominator)
+    override def +(that : Fraction[T]) = new Fraction(numerator * that.denominator + that.numerator * denominator, denominator * that.denominator)
     
-    override def *(that : Fraction[T]) = new Fraction(ring)(numerator * that.numerator, denominator * that.denominator)
+    override def *(that : Fraction[T]) = new Fraction(numerator * that.numerator, denominator * that.denominator)
     
-    override def negation() = new Fraction(ring)(-numerator, denominator)
+    override def negation() = new Fraction(-numerator, denominator)
     
     override def equals(that : Any) : Boolean = that match {
         case _ : Fraction[T] => {
@@ -40,4 +41,4 @@ object Rationals extends Fractions[Integer] (Integers) {
 }
 
 case class Rational (override val numerator : Integer, override val denominator : Integer = Integers.one) extends 
-    Fraction[Integer] (Integers)(numerator, denominator)
+    Fraction[Integer] (numerator, denominator)(Integers)
