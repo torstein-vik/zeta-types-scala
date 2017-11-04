dca960eb860fbcf48873fda47719d03f9ae0460b
Torstein Vik <torsteinv64@gmail.com>
2017-11-05 00:40:43 +0100

    Moved functionality out of DSL implicit class, refactored

--- src/main/scala/sequences/DSL.scala
+++ src/main/scala/sequences/DSL.scala
@@ -17,29 +17,8 @@ package object DSL {
     implicit class SequenceFactoryDSL[T, S] (val s : S)(implicit ev: S => SequenceFactory[T]) {
         lazy val old = ev(s)
         
-        def followedBy (next : SequenceFactory[T]) : SequenceFactory[T] = new SequenceFactory[T]{
-            def apply (seq : Sequence[T])(index : Int) : T = old.length match {
-                case None => old(seq)(index)
-                case Some(limit) if index <  limit =>  old(seq)(index)
-                case Some(limit) if index >= limit => next(seq)(index)
-            }
-            
-            override def hasInternalCache : Boolean = old.hasInternalCache && next.hasInternalCache
-            override def length : Option[Int] = for {l1 <- old.length; l2 <- next.length} yield l1 + l2
-        }
-        
-        def upTo (limit : Int) : SequenceFactory[T] = new SequenceFactory[T]{
-            def apply (seq : Sequence[T])(index : Int) : T = old.apply(seq)(index)
-            override def hasInternalCache : Boolean = old.hasInternalCache
-            override def length : Option[Int] = Some(limit + 1)
-        }
-        
-        def repeat : SequenceFactory[T] = new SequenceFactory[T]{
-            def apply (seq : Sequence[T])(index : Int) : T = old.length match {
-                case None => old.apply(seq)(index)
-                case Some(limit) => old.apply(seq)(index % limit)
-            }
-            override def hasInternalCache : Boolean = old.hasInternalCache
-        } 
+        def followedBy (next : SequenceFactory[T]) : SequenceFactory[T] = new JuxtapositionedSequence(old, next)
+        def upTo (limit : Int) : SequenceFactory[T] = new SequenceLimited(old, limit)
+        def repeat : SequenceFactory[T] = new RepeatedSequence[T](old)
     }
 }
--- src/main/scala/sequences/SequenceFactories.scala
+++ src/main/scala/sequences/SequenceFactories.scala
@@ -17,3 +17,29 @@ class ConstantSequence[E] (value : E) extends SequenceFactory[E] {
     def apply (seq : Sequence[E])(index : Int) = value
     override def hasInternalCache = true
 }
+
+class SequenceLimited[E] (old : SequenceFactory[E], limit : Int) extends SequenceFactory[E] {
+    def apply(seq : Sequence[E])(index : Int) = old.apply(seq)(index)
+    override def hasInternalCache : Boolean = old.hasInternalCache
+    override def length : Option[Int] = Some(limit + 1)
+}
+
+class RepeatedSequence[E] (old : SequenceFactory[E]) extends SequenceFactory[E] {
+    def apply (seq : Sequence[E])(index : Int) : E = old.length match {
+        case None => old.apply(seq)(index)
+        case Some(limit) => old.apply(seq)(index % limit)
+    }
+    
+    override def hasInternalCache : Boolean = old.hasInternalCache
+} 
+
+class JuxtapositionedSequence[E] (first : SequenceFactory[E], second : SequenceFactory[E]) extends SequenceFactory[E] {
+    def apply (seq : Sequence[E])(index : Int) : E = first.length match {
+        case None => first(seq)(index)
+        case Some(limit) if index <  limit =>  first(seq)(index)
+        case Some(limit) if index >= limit => second(seq)(index)
+    }
+    
+    override def hasInternalCache : Boolean = first.hasInternalCache && second.hasInternalCache
+    override def length : Option[Int] = for {l1 <- first.length; l2 <- second.length} yield l1 + l2
+}
