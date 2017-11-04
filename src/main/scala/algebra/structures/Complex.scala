e16831fb5e1ce07ac6ddfe8b2f4707ed9d9eaedd
Torstein Vik <torsteinv64@gmail.com>
2017-11-04 14:29:23 +0100

    Fixed wrong identities

--- src/main/scala/algebra/structures/Complex.scala
+++ src/main/scala/algebra/structures/Complex.scala
@@ -4,8 +4,8 @@ import org.zetatypes.algebra._
 
 case class ComplexNumbers[T <: RingElement[T]] (ring : Ring[T]) extends 
     RingClass[ComplexNumber[T]](
-    new ComplexNumber(ring)(ring.zero, ring.one), 
-    new ComplexNumber(ring)(ring.one,  ring.one))
+    new ComplexNumber(ring)(ring.zero, ring.zero), 
+    new ComplexNumber(ring)(ring.one,  ring.zero))
 
 class ComplexNumber[T <: RingElement[T]] (val ring : Ring[T])(val real : T, val imaginary : T = ring.zero) extends RingElement[ComplexNumber[T]]{
     
