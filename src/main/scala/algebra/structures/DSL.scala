c30b5db9781f3283affd23ba3f815932b1b82e60
Torstein Vik <torsteinv64@gmail.com>
2017-11-04 16:52:49 +0100

    Implemented new Complex DSL

--- src/main/scala/algebra/structures/DSL.scala
+++ src/main/scala/algebra/structures/DSL.scala
@@ -9,6 +9,13 @@ package object DSL {
     implicit def intToRational (x : Int) : Rational = Rational(x)
     implicit def intToComplex  (x : Int) : Complex  = Complex(x)
     
+    implicit def IntegerToRational (x : Integer) : Rational = Rational(x)
+    implicit def IntegerToComplex  (x : Integer) : Complex  = Complex(x)
+    
+    implicit def RationalToComplex  (x : Rational) : Complex  = Complex(x)
+    
+    def I = Complex(0, 1)
+    
     implicit def toRational(q : Fraction[Integer]) : Rational = Rational(q.numerator, q.denominator)
     implicit def toComplex(q : ComplexNumber[Fraction[Integer]]) : Complex = Complex(q.real, q.imaginary)
         
