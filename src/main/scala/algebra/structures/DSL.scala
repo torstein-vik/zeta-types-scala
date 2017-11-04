1ca6c7043fe9a66d98249d5ae19074d673bcc7d8
Torstein Vik <torsteinv64@gmail.com>
2017-11-04 14:55:33 +0100

    Added Fraction[Integer] => Rational implicit DSL def

--- src/main/scala/algebra/structures/DSL.scala
+++ src/main/scala/algebra/structures/DSL.scala
@@ -7,5 +7,8 @@ import org.zetatypes.algebra._
 package object DSL {
     implicit def toInteger (x : Int) : Integer = Integer(x)
     
+    
+    implicit def toRational(q : Fraction[Integer]) : Rational = Rational(q.numerator, q.denominator)
+        
     implicit def multiplicativeMonoid[E <: RingElement[E]] (ring : Ring[E]) : Monoid[E] = ring.multiplicative
 }
