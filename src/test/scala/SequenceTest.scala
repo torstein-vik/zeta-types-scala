f9e060209c20b19d9a0f6daafe96aa1adf2530ee
Torstein Vik <torsteinv64@gmail.com>
2017-11-04 23:51:42 +0100

    Added basic test of CachedSequence, for i => i

--- src/test/scala/SequenceTest.scala
+++ src/test/scala/SequenceTest.scala
@@ -6,5 +6,18 @@ class SequenceFactoryTest extends FunSuite {
     
     {// CachedSequence test
         import org.zetatypes.sequences.DSL._
+        
+        test ("CachedSequence works") {
+            val seq = new CachedSequence((i : Int) => i)
+            
+            (0 to 100) foreach {i => 
+                assert(seq(i) == i)
+            }
+            
+            (0 to 100) foreach {i => 
+                assert(seq(i) == i)
+            }
+        }
+        
     }
 }
