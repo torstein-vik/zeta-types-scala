8b3520a82de711a9044194519d6db23b69a8d864
Torstein Vik <torsteinv64@gmail.com>
2017-11-04 23:57:39 +0100

    Added implicit sequence DSL (Int => T) => SequenceFactory[T]

--- src/main/scala/sequences/DSL.scala
+++ src/main/scala/sequences/DSL.scala
@@ -3,4 +3,8 @@ package org.zetatypes.sequences
 import scala.language.implicitConversions
 
 package object DSL {
+    implicit def createSequenceFactory[T] (f : Int => T) : SequenceFactory[T] = new SequenceFactory[T] {
+        def apply(seq : Sequence[T])(index : Int) = f(index)
+    }
+    
 }
