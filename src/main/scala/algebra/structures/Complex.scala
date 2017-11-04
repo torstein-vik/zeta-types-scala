da35f93ee1e17af53029b377ad6161fd641f3af3
Torstein Vik <torsteinv64@gmail.com>
2017-11-04 21:55:43 +0100

    refactored complex toString, clean code now

--- src/main/scala/algebra/structures/Complex.scala
+++ src/main/scala/algebra/structures/Complex.scala
@@ -26,16 +26,11 @@ class ComplexNumber[T <: RingElement[T]] (val real : T, val imaginary : T)(impli
         case _ => false
     }
     
-    override def toString : String = {
-        if (imaginary == ring.zero) {
-            return real.toString
-        } else if (real == ring.zero && imaginary == ring.one) {
-            return "i"
-        } else if (real == ring.zero) {
-            return imaginary.toString + " i"
-        } else {
-            return real.toString + " + " + imaginary.toString + " i"
-        }
+    override def toString : String = (real, imaginary) match {
+        case (ring.zero, ring.one) => "i"
+        case (x, ring.zero) => x.toString
+        case (ring.zero, y) => y.toString + " i"
+        case (x, y) => x + " + " + y + " i"
     }
 }
 
