f7636255e7f2faabc0166135d286bd4f130ed3bd
Torstein Vik <torsteinv64@gmail.com>
2017-11-05 18:51:44 +0100

    Added identities and zero&one test

--- src/test/scala/AlgebraicSequenceTest.scala
+++ src/test/scala/AlgebraicSequenceTest.scala
@@ -9,5 +9,14 @@ import org.zetatypes.algebra.structures.DSL._
 class AlgebraicSequenceTest extends FunSuite {
     
     
+    test ("identities test") {
+        assert(ComplexSequences.additive.identity.createSeq(0, 10)       === Seq[Complex](0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+        assert(ComplexSequences.multiplicative.identity.createSeq(0, 10) === Seq[Complex](1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
+    }
+    
+    test ("zero and one test") {
+        assert(ComplexSequences.zero.createSeq(0, 10) === Seq[Complex](0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+        assert(ComplexSequences.one.createSeq(0, 10)  === Seq[Complex](1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
+    }
     
 }
