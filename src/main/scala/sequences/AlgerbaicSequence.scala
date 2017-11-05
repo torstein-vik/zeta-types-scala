87abb7d605d77bb7bfad3b670a2d8fe71c182c21
Torstein Vik <torsteinv64@gmail.com>
2017-11-05 18:52:32 +0100

    Fixed Algebraic Sequence additive identity

--- src/main/scala/sequences/AlgerbaicSequence.scala
+++ src/main/scala/sequences/AlgerbaicSequence.scala
@@ -6,8 +6,8 @@ import org.zetatypes.algebra.structures.{Complex}
 
 case class AlgebraicSequences[E <: RingElement[E]] (ring : Ring[E]) extends 
     RingClass[AlgebraicSequence[E]](
-    new AlgebraicSequence(new JuxtapositionedSequence(new SequenceLimited(new ConstantSequence(ring.zero), 0), new ConstantSequence(ring.one)))(ring),
-    new AlgebraicSequence(new ConstantSequence(ring.one))(ring))
+    new AlgebraicSequence(new ConstantSequence(ring.zero))(ring),
+    new AlgebraicSequence(new ConstantSequence(ring.one ))(ring))
 
 class AlgebraicSequence[E <: RingElement[E]] (factory : SequenceFactory[E])(implicit ring : Ring[E]) extends CachedSequence[E](factory) 
     with RingElement[AlgebraicSequence[E]] {
