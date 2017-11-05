41170cad018497b37cf7a604f64723396416fd36
Torstein Vik <torsteinv64@gmail.com>
2017-11-05 18:10:04 +0100

    Added algebraic method stubs for Algebraic Sequence

--- src/main/scala/sequences/AlgerbaicSequence.scala
+++ src/main/scala/sequences/AlgerbaicSequence.scala
@@ -3,7 +3,7 @@ package org.zetatypes.sequences
 import org.zetatypes.algebra._
 import org.zetatypes.algebra.structures.DSL.{Rational}
 
-/*
+
 case class AlgebraicSequences[E <: RingElement[E]] (ring : Ring[E]) extends 
     RingClass[AlgebraicSequence[E]](
     new AlgebraicSequence(new JuxtapositionedSequence(new SequenceLimited(new ConstantSequence(ring.zero), 0), new ConstantSequence(ring.one)))(ring),
@@ -11,6 +11,11 @@ case class AlgebraicSequences[E <: RingElement[E]] (ring : Ring[E]) extends
 
 class AlgebraicSequence[E <: RingElement[E]] (factory : SequenceFactory[E])(implicit ring : Ring[E]) extends CachedSequence[E](factory) 
     with RingElement[AlgebraicSequence[E]] {
+    override lazy val canonicalRing = AlgebraicSequences(ring)
     
+    override def +(that : AlgebraicSequence[E]) = null
+    
+    override def *(that : AlgebraicSequence[E]) = null
+    
+    override def negation() = null
 }
-*/
