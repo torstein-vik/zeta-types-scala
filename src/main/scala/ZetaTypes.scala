0569ddcb3edd5f95347d8386eda89f59c5051b41
Torstein Vik <torsteinv64@gmail.com>
2017-11-05 23:55:07 +0100

    Added two package objects, ZetaType and ZetaTypeDSL

--- src/main/scala/ZetaTypes.scala
+++ src/main/scala/ZetaTypes.scala
@@ -1,6 +1,14 @@
 package org.torsteinv
 
-package object ZetaType 
-    //extends zetatypes.tannakiansymbols.DSLTrait
-    //with zetatypes.sequences.DSLTrait
-    //with zetatypes.algebra.structures.DSLTrait
+trait ProjectPackage 
+    extends zetatypes.algebraPackage
+    with zetatypes.algebra.structuresPackage
+    with zetatypes.sequencesPackage
+    with zetatypes.tannakiansymbolsPackage
+
+package object ZetaType extends ProjectPackage
+
+package object ZetaTypeDSL extends ProjectPackage
+    with zetatypes.algebra.structures.DSLPackaging
+    with zetatypes.tannakiansymbols.DSLPackaging
+    with zetatypes.sequences.DSLPackaging
