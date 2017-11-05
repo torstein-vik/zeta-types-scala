1e54bff632c9a4eee1ed1c6e9ebbdb3433cd4d48
Torstein Vik <torsteinv64@gmail.com>
2017-11-05 22:59:43 +0100

    Packaged TS and Multiset

---
+++ src/main/scala/tannakiansymbols/TannakianSymbolic.scala
@@ -0,0 +1,9 @@
+package org.torsteinv.zetatypes
+
+import tannakiansymbols._
+
+trait tannakiansymbolic
+    extends MultisetlikePackage
+    with TannakianSymbolicPackage
+
+package object tannakiansymbols extends tannakiansymbolic
