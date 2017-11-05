356da94ec2907ee6bd9701fdb8d2ab5771bbec71
Torstein Vik <torsteinv64@gmail.com>
2017-11-05 20:15:10 +0100

    Added classes for spec. alg. seq.

--- src/main/scala/sequences/SpecialAlgebraicSequence.scala
+++ src/main/scala/sequences/SpecialAlgebraicSequence.scala
@@ -4,3 +4,12 @@ import org.zetatypes.algebra._
 import org.zetatypes.algebra.structures.{Complex}
 
 
+case class SpecialAlgebraicSequences[E <: RingElement[E]] (ring : Ring[E]) extends 
+    RingClass[SpecialAlgebraicSequence[E]](
+    new SpecialAlgebraicSequence(new ConstantSequence(ring.zero))(ring),
+    new SpecialAlgebraicSequence(new ConstantSequence(ring.one ))(ring))
+
+class SpecialAlgebraicSequence[E <: RingElement[E]] (factory : SequenceFactory[E])(implicit ring : Ring[E]) extends CachedSequence[E](factory) 
+    with RingElement[SpecialAlgebraicSequence[E]] {
+}
+
