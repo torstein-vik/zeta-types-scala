b8b1c7116e4412b5f0fafe67f7047778c0b41c1a
Torstein Vik <torsteinv64@gmail.com>
2017-11-05 21:51:13 +0100

    Made ZetaType extend DSL traits

--- src/main/scala/Package.scala
+++ src/main/scala/Package.scala
@@ -1,5 +1,6 @@
 package org.torsteinv
 
-package object ZetaType {
-    
-}
+package object ZetaType 
+    extends zetatypes.tannakiansymbols.DSLTrait
+    with zetatypes.sequences.DSLTrait
+    with zetatypes.algebra.structures.DSLTrait
