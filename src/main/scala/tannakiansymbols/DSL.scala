883de2f5237e1ee77e2a67423e07439898f876c2
Torstein Vik <torsteinv64@gmail.com>
2017-11-05 22:56:12 +0100

    Renamed DSLTrait => DSLPackage

--- src/main/scala/tannakiansymbols/DSL.scala
+++ src/main/scala/tannakiansymbols/DSL.scala
@@ -2,9 +2,9 @@ package org.torsteinv.zetatypes.tannakiansymbols
 
 import scala.language.implicitConversions
 
-package object DSL extends DSLTrait
+package object DSL extends DSLPackage
 
-trait DSLTrait {
+trait DSLPackage {
     implicit def toMS[T] (x : T) : Multiset[T] = new Multiset(x)
     
     def ms[T] (x : T*) : Multiset[T] = new Multiset(x : _*)
