39550c1de9c16792e25dc332c0bd9940309ed017
Torstein Vik <torsteinv64@gmail.com>
2017-11-05 14:26:24 +0100

    Added (deactivated) basic algebraic definitions of Algebraic Sequence

---
+++ src/main/scala/sequences/AlgerbaicSequence.scala
@@ -0,0 +1,16 @@
+package org.zetatypes.sequences
+
+import org.zetatypes.algebra._
+import org.zetatypes.algebra.structures.DSL.{Rational}
+
+/*
+case class AlgebraicSequences[E <: RingElement[E]] (ring : Ring[E]) extends 
+    RingClass[AlgebraicSequence[E]](
+    new AlgebraicSequence(new JuxtapositionedSequence(new SequenceLimited(new ConstantSequence(ring.zero), 0), new ConstantSequence(ring.one)))(ring),
+    new AlgebraicSequence(new ConstantSequence(ring.one))(ring))
+
+class AlgebraicSequence[E <: RingElement[E]] (factory : SequenceFactory[E])(implicit ring : Ring[E]) extends CachedSequence[E](factory) 
+    with RingElement[AlgebraicSequence[E]] {
+    
+}
+*/
