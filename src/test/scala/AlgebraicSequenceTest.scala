d697c37fe7e3f1d178f0edac08fbf43ea12fa66c
Torstein Vik <torsteinv64@gmail.com>
2017-11-05 18:35:00 +0100

    Added AlgebraicSequenceTest

---
+++ src/test/scala/AlgebraicSequenceTest.scala
@@ -0,0 +1,13 @@
+import org.scalatest.FunSuite
+
+import org.zetatypes.sequences._
+import org.zetatypes.sequences.DSL._
+import org.zetatypes.algebra._
+import org.zetatypes.algebra.structures._
+import org.zetatypes.algebra.structures.DSL._
+
+class AlgebraicSequenceTest extends FunSuite {
+    
+    
+    
+}
