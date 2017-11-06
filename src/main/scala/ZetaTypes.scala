4792d50508c176f1d5ba18853766d4cfea73909a
Torstein Vik <torsteinv64@gmail.com>
2017-11-06 18:14:37 +0100

    Deleted packaging structure

--- src/main/scala/ZetaTypes.scala
+++ src/main/scala/ZetaTypes.scala
@@ -1,14 +1,2 @@
 package org.torsteinv
 
-trait ProjectPackage 
-    extends zetatypes.algebraPackage
-    with zetatypes.algebra.structuresPackage
-    with zetatypes.sequencesPackage
-    with zetatypes.tannakiansymbolsPackage
-
-package object ZetaType extends ProjectPackage
-
-package object ZetaTypeDSL extends ProjectPackage
-    with zetatypes.algebra.structures.DSLPackaging
-    with zetatypes.tannakiansymbols.DSLPackaging
-    with zetatypes.sequences.DSLPackaging
--- src/main/scala/tannakiansymbols/TannakianSymbolsPackage.scala
+++
@@ -1,9 +0,0 @@
-package org.torsteinv.zetatypes
-
-import tannakiansymbols._
-
-trait tannakiansymbolsPackage
-    extends MultisetPackaging
-    with TannakianSymbolsPackaging
-
-package object tannakiansymbols extends tannakiansymbolsPackage
