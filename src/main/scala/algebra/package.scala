1a7bcfe0f9219ad41dc70f57d0a3fdda49231463
torstein-vik <torsteinv64@gmail.com>
2017-11-07 16:50:47 +0100

    Wrote doc of algebra package

--- src/main/scala/algebra/package.scala
+++ src/main/scala/algebra/package.scala
@@ -1,6 +1,9 @@
 package org.torsteinv.zetatypes
 
-/** 
+/** Provides traits for algebraic structures, and a subpackage [[structures]]
+ *  with some commonly used structures. Some traits have functionality
+ *  implemented, such as [[STDLambdaRingElement]], which defines the lambda
+ *  operations given the Adams operations
  *  
  */
 package object algebra {}
