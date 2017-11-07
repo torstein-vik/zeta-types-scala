3d9d04387f7f044b5c624c4c9843bc4709e49ba2
torstein-vik <torsteinv64@gmail.com>
2017-11-07 18:08:39 +0100

    Added doc for sequences package

--- src/main/scala/sequences/package.scala
+++ src/main/scala/sequences/package.scala
@@ -2,8 +2,9 @@ package org.torsteinv.zetatypes
 
 import org.torsteinv.zetatypes.algebra.structures.{Complex}
 
-/** 
- *  
+/** Provides classes for creating and working with (potentially infinite) 
+ *  sequences, as well as algebraic instances for normal and special sequences.
+ *  Also provides some implicit methods for simplifying syntax.
  */
 package object sequences {
     type ComplexSequence = AlgebraicSequence[Complex]
