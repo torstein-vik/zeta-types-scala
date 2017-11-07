a057c4c3f17cf08018c98d4e077fa3409a6aea50
torstein-vik <torsteinv64@gmail.com>
2017-11-07 17:27:45 +0100

    Added scaladoc to structures package

--- src/main/scala/algebra/structures/package.scala
+++ src/main/scala/algebra/structures/package.scala
@@ -1,7 +1,10 @@
 package org.torsteinv.zetatypes.algebra
 
-/** 
- *  
+/** Provides instances of various algerbraic structures, such as [[ComplexNumber]]
+ *  , [[Fractions]] (for any ring), and the [[Integers]]. Also has the 
+ *  subpackage [[DSL]], which houses various implicit converters for simplifying
+ *  syntax. Also provides type synonyms [[Rational]] = [[Fraction[Integer]]],
+ *  and [[Complex]] = [[ComplexNumber]][[[Rational]]]
  */
 package object structures {
     type Rational = Fraction[Integer]
