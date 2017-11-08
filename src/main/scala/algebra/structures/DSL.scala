ccecc6367cb6e8259c2c419f4805f26768b6adec
torstein-vik <torsteinv64@gmail.com>
2017-11-08 13:09:29 +0100

    Added doc to structures DSL conversion methods

--- src/main/scala/algebra/structures/DSL.scala
+++ src/main/scala/algebra/structures/DSL.scala
@@ -9,13 +9,19 @@ import org.torsteinv.zetatypes.algebra._
  *  is the imaginary unit, and the implicit [[DSL.multiplicativeMonoid]]
  */
 package object DSL {
+    /** implicitly converts an int in standard syntax to an Integer */
     implicit def intToInteger  (x : Int) : Integer  = Integer(x)
+    /** implicitly converts an int in standard syntax to a Rational number */
     implicit def intToRational (x : Int) : Rational = Rational(x)
+    /** implicitly converts an int in standard syntax to a Complex number */
     implicit def intToComplex  (x : Int) : Complex  = Complex(x)
     
+    /** implicitly converts an Integer to a Rational */
     implicit def IntegerToRational (x : Integer) : Rational = Rational(x)
+    /** implicitly converts an Integer to a Complex number */
     implicit def IntegerToComplex  (x : Integer) : Complex  = Complex(x)
     
+    /** implicitly converts a Rational number to a Complex number */
     implicit def RationalToComplex  (x : Rational) : Complex  = Complex(x)
     
     def I = Complex(0, 1)
