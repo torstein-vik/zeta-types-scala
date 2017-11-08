3b309fd9367fc76d3a3635d4affba7e1ca61774b
torstein-vik <torsteinv64@gmail.com>
2017-11-08 12:59:42 +0100

    Added TS DSL doc

--- src/main/scala/tannakiansymbols/DSL.scala
+++ src/main/scala/tannakiansymbols/DSL.scala
@@ -2,6 +2,12 @@ package org.torsteinv.zetatypes.tannakiansymbols
 
 import scala.language.implicitConversions
 
+/** Provides methods for creating [[Multiset]]s, which in turn can be used to create
+ *  Tannakian symbols. Multisets can always be created easily, but to covert it
+ *  into a Tannakian symbol, there needs to be an implicit 
+ *  [[org.torsteinv.zetatypes.algebra.Monoid]] in the scope.
+ *  This is explained further in [[Multiset./]]
+ */
 package object DSL {
     implicit def toMS[T] (x : T) : Multiset[T] = new Multiset(x)
     
