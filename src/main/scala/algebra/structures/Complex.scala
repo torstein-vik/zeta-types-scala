3312ab0e79e420311a92b841ed52d23a87f3d8b1
Torstein Vik <torsteinv64@gmail.com>
2017-11-10 12:36:56 +0100

    Added doc to Complex.scala

--- src/main/scala/algebra/structures/Complex.scala
+++ src/main/scala/algebra/structures/Complex.scala
@@ -3,12 +3,23 @@ package org.torsteinv.zetatypes.algebra.structures
 import org.torsteinv.zetatypes.algebra._
 
 
-
+/** A ring where the elements are x + y*i where x and y are in some ring. An important special case is [[Complex]] = ComplexNumbers([[Rationals]])
+ *  @tparam T the type of [[RingElement]] that the real and imaginary part of this ring's elements belong to.
+ *  @constructor Creates a new ring of complex numbers with coefficients in some ring.
+ *  @param ring The [[Ring]] that the coefficients belong to.
+ */
 case class ComplexNumbers[T <: RingElement[T]] (ring : Ring[T]) extends 
     RingClass[ComplexNumber[T]](
     new ComplexNumber(ring.zero, ring.zero)(ring), 
     new ComplexNumber(ring.one,  ring.zero)(ring))
 
+/** An element of [[ComplexNumbers]]
+ *  @tparam T the type of [[RingElement]] that the real and imaginary part of this element belong to.
+ *  @constructor Creates a new complex number from a real and an imaginary part
+ *  @param real The real part of this complex number
+ *  @param imaginary The imaginary part of this complex number
+ *  @param ring The ring that the real and imaginary parts belong to
+ */
 class ComplexNumber[T <: RingElement[T]] (val real : T, val imaginary : T)(implicit val ring : Ring[T]) extends RingElement[ComplexNumber[T]]{
     
     override lazy val canonicalRing = ComplexNumbers(ring)
@@ -36,7 +47,12 @@ class ComplexNumber[T <: RingElement[T]] (val real : T, val imaginary : T)(impli
     }
 }
 
+/** An object to create a [[ComplexNumber]] with [[Rational]] coefficients.
+ *  Also provides unapply, so that ComplexNumbers can be pattern matched.
+ */
 object Complex extends ComplexNumbers (Rationals) {
+    /** Creates a new [[Rational]] [[ComplexNumber]] using a real and imaginary part as parameters */
     def apply(real : Rational, imaginary : Rational = Rationals.zero) = new ComplexNumber(real, imaginary)(Rationals)
+    /** Takes a ([[Rational]]) [[ComplexNumber]] and extracts its real and imaginary parts */
     def unapply(z : ComplexNumber[Rational]) : Option[(Rational, Rational)] = Some(z.real, z.imaginary)
 }
