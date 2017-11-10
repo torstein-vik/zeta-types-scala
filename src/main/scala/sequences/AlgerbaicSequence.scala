c3b530b382815387b67b89f8fe2fefc388b4a817
Torstein Vik <torsteinv64@gmail.com>
2017-11-10 16:59:09 +0100

    Added doc to AlgebraicSequences.scala

--- src/main/scala/sequences/AlgerbaicSequence.scala
+++ src/main/scala/sequences/AlgerbaicSequence.scala
@@ -4,11 +4,22 @@ import org.torsteinv.zetatypes.algebra._
 import org.torsteinv.zetatypes.algebra.structures.{Complex}
 
 
+/** A ring where the elements are [[AlgebraicSequence]]s with elements in some given [[org.torsteinv.zetatypes.algebra.Ring]] 
+ *  @tparam E the type of [[org.torsteinv.zetatypes.algebra.RingElement]] that the [[AlgebraicSequence]]s consist of
+ *  @constructor Creates a new ring of [[AlgebraicSequence]]s with elements in a given [[org.torsteinv.zetatypes.algebra.Ring]] 
+ *  @param ring The [[org.torsteinv.zetatypes.algebra.Ring]] that the [[AlgebraicSequence]] elements belong to
+ */
 case class AlgebraicSequences[E <: RingElement[E]] (ring : Ring[E]) extends 
     RingClass[AlgebraicSequence[E]](
     new AlgebraicSequence(new ConstantSequence(ring.zero))(ring),
     new AlgebraicSequence(new ConstantSequence(ring.one ))(ring))
-
+    
+/** An element of [[AlgebraicSequences]], and a [[CachedSequence]]
+ *  @tparam T the type of [[org.torsteinv.zetatypes.algebra.RingElement]] that the elements of this sequence belong to
+ *  @constructor Creates a new [[CachedSequence]] from a [[SequenceFactory]]
+ *  @param factory The [[SequenceFactory]] that this sequence is created from
+ *  @param ring The [[org.torsteinv.zetatypes.algebra.Ring]] that the elements of this [[CachedSequence]] belong to
+ */
 class AlgebraicSequence[E <: RingElement[E]] (factory : SequenceFactory[E])(implicit ring : Ring[E]) extends CachedSequence[E](factory) 
     with RingElement[AlgebraicSequence[E]] {
     override lazy val canonicalRing = AlgebraicSequences(ring)
@@ -20,8 +31,12 @@ class AlgebraicSequence[E <: RingElement[E]] (factory : SequenceFactory[E])(impl
     override def negation() = new AlgebraicSequence(new MappedSequence(this)(_.negation)(true))
 }
 
+/** A ring of [[AlgebraicSequence]]s with elements in the [[org.torsteinv.zetatypes.structures.Complex]] numbers
+ */
 object ComplexSequences extends AlgebraicSequences(Complex)
 
+/** An object to create an [[AlgebraicSequence]] with [[org.torsteinv.zetatypes.structures.Complex]] elements.
+ */
 object ComplexSequence{
     def apply (factory : SequenceFactory[Complex]) = new AlgebraicSequence(factory)(Complex)
 }
