e34b7f20c5c55e0f2983d3e715d07aaf5c4a4c49
Torstein Vik <torsteinv64@gmail.com>
2017-11-14 20:17:15 +0100

    Added Polynomials doc

--- src/main/scala/algebra/structures/Polynomial.scala
+++ src/main/scala/algebra/structures/Polynomial.scala
@@ -2,6 +2,11 @@ package org.torsteinv.zetatypes.algebra.structures
 
 import org.torsteinv.zetatypes.algebra._
 
+/** A ring where the elements are [[Polynomial]]s with elements in some given [[org.torsteinv.zetatypes.algebra.Ring]] 
+ *  @tparam T the type of [[org.torsteinv.zetatypes.algebra.RingElement]] that the [[Polynomial]]s consist of
+ *  @constructor Creates a new ring of [[Polynomial]]s with coefficients in a given [[org.torsteinv.zetatypes.algebra.Ring]] 
+ *  @param ring The [[org.torsteinv.zetatypes.algebra.Ring]] that the [[Polynomial]] coefficients belong to
+ */
 case class Polynomials[E <: RingElement](ring : Ring[E]) extends 
     RingClass[Polynomials[E]](
     new Polynomial(Seq.empty)(monoid), 
