9a6aefb67f37f70d87ea162ec7e388828ccb8d91
Torstein Vik <torsteinv64@gmail.com>
2017-11-14 20:16:59 +0100

    Added case class Polynomials

--- src/main/scala/algebra/structures/Polynomial.scala
+++ src/main/scala/algebra/structures/Polynomial.scala
@@ -1,2 +1,8 @@
 package org.torsteinv.zetatypes.algebra.structures
 
+import org.torsteinv.zetatypes.algebra._
+
+case class Polynomials[E <: RingElement](ring : Ring[E]) extends 
+    RingClass[Polynomials[E]](
+    new Polynomial(Seq.empty)(monoid), 
+    new Polynomial(Seq((monoid.identity, 0)))(monoid))
