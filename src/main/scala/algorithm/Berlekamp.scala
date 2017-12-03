8cb853ebe2258c2b439b8ae2ea3fa1a515dbb81a
Torstein Vik <torsteinv64@gmail.com>
2017-12-03 18:04:15 +0100

    Added container type for amount of data taken into account in BK

--- src/main/scala/algorithm/Berlekamp.scala
+++ src/main/scala/algorithm/Berlekamp.scala
@@ -1,6 +1,7 @@
 package io.github.torsteinvik.zetatypes.algorithm
 
 package object Berlekamp {
+    case class BerlekampSpec(length : Int)
     case class BerlekampResult[T](recursion : Seq[T], initialdata : Seq[T])
     
 }
