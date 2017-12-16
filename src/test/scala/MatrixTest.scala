f6282685a3f0eac6a7ab3465c4668cf5f54bbb69
Torstein Vik <torsteinv64@gmail.com>
2017-12-16 20:32:33 +0100

    Added examples of matricies to test suite

--- src/test/scala/MatrixTest.scala
+++ src/test/scala/MatrixTest.scala
@@ -3,4 +3,22 @@ package io.github.torsteinvik.zetatypes.test
 import org.scalatest.FunSuite
 
 class MatrixTest extends FunSuite{
+    import io.github.torsteinvik.zetatypes.algebra.structures._
+    import io.github.torsteinvik.zetatypes.algebra._
+    
+    
+    {// Most basic tests
+        implicit val ring : Ring[Integer] = Integers
+        
+        val X     = new Matrix(1, 2, Seq(Seq(1, 2)))
+        val Xcopy = new Matrix(1, 2, Seq(Seq(1, 2)))
+        val Y     = new Matrix(3, 3, Seq(Seq(1, 2, 3), Seq(2, 3, 4), Seq(3, 4, 5))
+        val Ycopy = new Matrix(3, 3, Seq(Seq(1, 2, 3), Seq(2, 3, 4), Seq(3, 4, 5))
+        val Z     = new Matrix(1000, 1000, for {i <- 1 to 1000} yield for {j <- 1 to 1000} yield i * j + 2)
+        val Zcopy = new Matrix(1000, 1000, for {i <- 1 to 1000} yield for {j <- 1 to 1000} yield i * j + 2)
+        
+        
+    }
+    
+    
 }
