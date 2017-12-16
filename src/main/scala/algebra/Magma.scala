5f9c31d9d5487372e0215ebd444f39be6e220445
Torstein Vik <torsteinv64@gmail.com>
2017-12-17 00:59:45 +0100

    Renamed RepitionAlgorithm => MagmaRepitionAlgorithm

--- src/main/scala/algebra/Magma.scala
+++ src/main/scala/algebra/Magma.scala
@@ -17,7 +17,7 @@ trait Magma[T <: AlgebraicElement] {
     /** Combine two elements of type T into a signle one, according to the structure of this [[Magma]] */
     def combine (x : T, y : T) : T
     /** Repeated combination of a single T, n times */
-    def repeated (x : T, n : Int) : T = RepetitionAlgorithm[T](combine, n, x, x)
+    def repeated (x : T, n : Int) : T = MagmaRepetitionAlgorithm[T](combine, n, x, x)
     
 }
 /** An [[AlgebraicElement]] with an addition defined on it
@@ -27,7 +27,7 @@ trait Additive[that <: Additive[that]] extends AlgebraicElement{
     /** Add some other element to this */
     def +(y : that) : that
     /** Add this Additive element to itself n times. Requires some implicit evidence that the type of this is a subtype of that*/
-    def ++(n : Int)(implicit ev: this.type <:< that) : that = RepetitionAlgorithm[that](_ + _, n, ev(this), ev(this))
+    def ++(n : Int)(implicit ev: this.type <:< that) : that = MagmaRepetitionAlgorithm[that](_ + _, n, ev(this), ev(this))
     
 }
 /** An [[AlgebraicElement]] with a multiplication defined on it
@@ -37,19 +37,19 @@ trait Multiplicative[that <: Multiplicative[that]] extends AlgebraicElement {
     /** Multiply some other element with this */
     def *(y : that) : that
     /** Multiply this Multiplicative element with itself n times. Requires some implicit evidence that the type of this is a subtype of that*/
-    def **(n : Int)(implicit ev: this.type <:< that) : that = RepetitionAlgorithm[that](_ * _, n, ev(this), ev(this))
+    def **(n : Int)(implicit ev: this.type <:< that) : that = MagmaRepetitionAlgorithm[that](_ * _, n, ev(this), ev(this))
     /** Synonym for [[**]] */
     def ~^(n : Int)(implicit ev: this.type <:< that) = this.**(n)(ev)
     
     
 }
 
-private object RepetitionAlgorithm {
+private object MagmaRepetitionAlgorithm {
     import scala.annotation.tailrec
         
     @tailrec
     def apply[T](f : (T, T) => T, n : Int, x : T, acc : T) : T = n match {
-        case _ if n > 1 => RepetitionAlgorithm(f, n - 1, x, f(x, acc))
+        case _ if n > 1 => MagmaRepetitionAlgorithm(f, n - 1, x, f(x, acc))
         case 1 => acc
         case _ => throw new AlgebraicException("Magmatic repeated multiplication requires n > 0")
     }
