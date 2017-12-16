0cf536ba3c15a9db933f45576bb763085721dbde
Torstein Vik <torsteinv64@gmail.com>
2017-12-16 20:31:28 +0100

    Added case class for ring of n by n matricies

--- src/main/scala/algebra/structures/Matrix.scala
+++ src/main/scala/algebra/structures/Matrix.scala
@@ -2,3 +2,7 @@ package io.github.torsteinvik.zetatypes.algebra.structures
 
 import io.github.torsteinvik.zetatypes.algebra._
 
+case class Matricies[T <: RingElement[T]] (ring : Ring[T], n : Int) extends 
+    RingClass[Matrix[T]](
+    new Matrix(n, n, for {i <- (1 to n)} yield for {j <- (1 to n)} yield ring.zero)(ring), 
+    new Matrix(n, n, for {i <- (1 to n)} yield for {j <- (1 to n)} yield if i == j ring.one else ring.zero)(ring))
