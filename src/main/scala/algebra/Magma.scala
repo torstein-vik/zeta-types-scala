7c986b999e03d9bdb695029bdc9446f4fa6798f3
Torstein Vik <torsteinv64@gmail.com>
2017-12-17 00:43:22 +0100

    Removed old repetion implementation in favor of new

--- src/main/scala/algebra/Magma.scala
+++ src/main/scala/algebra/Magma.scala
@@ -17,16 +17,8 @@ trait Magma[T <: AlgebraicElement] {
     /** Combine two elements of type T into a signle one, according to the structure of this [[Magma]] */
     def combine (x : T, y : T) : T
     /** Repeated combination of a single T, n times */
-    def repeated (x : T, n : Int) : T = n match {
-        case n if n > 0 => expTailRec(n, x, x)
-        case _ => throw new AlgebraicException("Magmatic repeated addition requires n > 0")
-    }
+    def repeated (x : T, n : Int) : T = RepetitionAlgorithm[T](combine, n, x, x)
     
-    @tailrec
-    private def expTailRec(n : Int, x : T, acc : T) : T = {
-        if (n <= 1) return acc
-        return expTailRec(n - 1, x, combine(x, acc))
-    }  
 }
 /** An [[AlgebraicElement]] with an addition defined on it
  *  @tparam that The type that this element may combined with and yield. that must be a subtype of Additive[that]
@@ -35,16 +27,8 @@ trait Additive[that <: Additive[that]] extends AlgebraicElement{
     /** Add some other element to this */
     def +(y : that) : that
     /** Add this Additive element to itself n times. Requires some implicit evidence that the type of this is a subtype of that*/
-    def ++(n : Int)(implicit ev: this.type <:< that) : that = n match {
-        case n if n > 0 => expTailRec(n, ev(this))
-        case _ => throw new AlgebraicException("Magmatic repeated addition requires n > 0")
-    }
+    def ++(n : Int)(implicit ev: this.type <:< that) : that = RepetitionAlgorithm[that](_ + _, n, ev(this), ev(this))
     
-    @tailrec
-    private def expTailRec(n : Int, acc : that) : that = {
-        if (n <= 1) return acc
-        return expTailRec(n - 1, this + acc)
-    } 
 }
 /** An [[AlgebraicElement]] with a multiplication defined on it
  *  @tparam that The type that this element may combined with and yield. that must be a subtype of Multiplicative[that]
@@ -53,18 +37,11 @@ trait Multiplicative[that <: Multiplicative[that]] extends AlgebraicElement {
     /** Multiply some other element with this */
     def *(y : that) : that
     /** Multiply this Multiplicative element with itself n times. Requires some implicit evidence that the type of this is a subtype of that*/
-    def **(n : Int)(implicit ev: this.type <:< that) : that = n match {
-        case n if n > 0 => expTailRec(n, ev(this))
-        case _ => throw new AlgebraicException("Magmatic repeated multiplication requires n > 0")
-    }
+    def **(n : Int)(implicit ev: this.type <:< that) : that = RepetitionAlgorithm[that](_ * _, n, ev(this), ev(this))
     
-    def ~^(n : Int)(implicit ev: this.type <:< that) : that = this.**(n)(ev)
+    def ~^(n : Int)(implicit ev: this.type <:< that) = this.**(n)(ev)
     
-    @tailrec
-    private def expTailRec(n : Int, acc : that) : that = {
-        if (n <= 1) return acc
-        return expTailRec(n - 1, this * acc)
-    } 
+    
 }
 
 private object RepetitionAlgorithm {
