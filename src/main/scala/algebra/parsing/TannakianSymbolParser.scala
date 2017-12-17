8452179a8bb53afc6995ed700393b3cd68353443
Torstein Vik <torsteinv64@gmail.com>
2017-12-18 00:26:48 +0100

    Implemented TS parser

--- src/main/scala/algebra/parsing/TannakianSymbolParser.scala
+++ src/main/scala/algebra/parsing/TannakianSymbolParser.scala
@@ -10,7 +10,10 @@ import io.github.torsteinvik.zetatypes.tannakiansymbols.DSL._
 object TannakianSymbolParser {
     import AlgebraicParser._
     /** Parses an algebraic [[io.github.torsteinvik.zetatypes.tannakiansymbols.TannakianSymbol]] from an input String */
-    def apply[T <: MonoidElement](element : Parser[T], monoid : Monoid[T]) : Parser[TannakianSymbol[T]] = ???
+    def apply[T <: MonoidElement](element : Parser[T], monoid : Monoid[T]) : Parser[TannakianSymbol[T]] = multiset(element, monoid) ~ ("/" ~> multiset(element, monoid)) ^^ {
+        case upstairs ~ downstairs => (ms(upstairs : _*) / ms(downstairs : _*))(monoid, implicitly)
+    }
+    
     private def multiset[T <: MonoidElement](element : Parser[T], monoid : Monoid[T]) : Parser[Seq[T]] = emptyset | ("{" ~> repsep(element, ",") <~ "}")
     
     private def emptyset[T] : Parser[Seq[T]] = "Ø" ^^^ Seq.empty[T]
