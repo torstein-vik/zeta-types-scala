4610696bc98ea9c9951b1f85d6a1f7351860683d
Torstein Vik <torsteinv64@gmail.com>
2017-12-18 00:26:26 +0100

    Added emptyset parser to TS parser

--- src/main/scala/algebra/parsing/TannakianSymbolParser.scala
+++ src/main/scala/algebra/parsing/TannakianSymbolParser.scala
@@ -12,4 +12,6 @@ object TannakianSymbolParser {
     /** Parses an algebraic [[io.github.torsteinvik.zetatypes.tannakiansymbols.TannakianSymbol]] from an input String */
     def apply[T <: MonoidElement](element : Parser[T], monoid : Monoid[T]) : Parser[TannakianSymbol[T]] = ???
     
+    private def emptyset[T] : Parser[Seq[T]] = "Ø" ^^^ Seq.empty[T]
+    
 }
