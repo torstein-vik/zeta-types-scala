f75e2dd018523db3dac8466e685ac9c7b042e373
Torstein Vik <torsteinv64@gmail.com>
2017-12-18 00:08:01 +0100

    Added TannakianSymbolParser.scala for parsing ts

---
+++ src/main/scala/algebra/parsing/TannakianSymbolParser.scala
@@ -0,0 +1,12 @@
+package io.github.torsteinvik.zetatypes.algebra.parsing
+
+import scala.util.parsing.combinator._
+
+import io.github.torsteinvik.zetatypes.algebra._
+import io.github.torsteinvik.zetatypes.tannakiansymbols._
+import io.github.torsteinvik.zetatypes.tannakiansymbols.DSL._
+
+object TannakianSymbolParser {
+    import AlgebraicParser._
+    
+}
