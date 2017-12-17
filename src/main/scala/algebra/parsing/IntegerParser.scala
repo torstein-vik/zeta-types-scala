9c84a9ac99375a8fd21797a85a7c085936420ed1
Torstein Vik <torsteinv64@gmail.com>
2017-12-17 15:13:51 +0100

    Added doc to integer parser

--- src/main/scala/algebra/parsing/IntegerParser.scala
+++ src/main/scala/algebra/parsing/IntegerParser.scala
@@ -4,7 +4,9 @@ import scala.util.parsing.combinator._
 
 import io.github.torsteinvik.zetatypes.algebra.structures._
 
+/** Parses an algebraic [[io.github.torsteinvik.zetatypes.algebra.structures.Integer]] from an input String */
 object IntegerParser{
     import AlgebraicParser._
+    /** Parses an algebraic [[io.github.torsteinvik.zetatypes.algebra.structures.Integer]] from an input String */
     def apply = """-?\d+""".r ^^ (str => Integer(BigInt(str)))
 }
