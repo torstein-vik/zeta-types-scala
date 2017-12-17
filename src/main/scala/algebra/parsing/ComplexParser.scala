1f77eba6125a374029adffc2f289564861e3dee2
Torstein Vik <torsteinv64@gmail.com>
2017-12-17 22:59:06 +0100

    Refactored complex parser to only use (T, T)

--- src/main/scala/algebra/parsing/ComplexParser.scala
+++ src/main/scala/algebra/parsing/ComplexParser.scala
@@ -9,26 +9,29 @@ import io.github.torsteinvik.zetatypes.algebra._
 object ComplexParser {
     import AlgebraicParser._
     /** Parses an algebraic [[io.github.torsteinvik.zetatypes.algebra.structures.ComplexNumber]] from an input String */
-    def apply[T <: RingElement[T]](element : Parser[T], ring : Ring[T]) : Parser[ComplexNumber[T]] = 
+    def apply[T <: RingElement[T]](element : Parser[T], ring : Ring[T]) : Parser[ComplexNumber[T]] = (
         imaginary(element, ring) |
         addition(element, ring) | 
         subtraction(element, ring) |
-        real(element, ring) 
+        real(element, ring)
+        ) ^^ {
+                case (real, imag) => new ComplexNumber(real, imag)(ring)
+        }
     
-    private def addition[T <: RingElement[T]](element : Parser[T], ring : Ring[T]) : Parser[ComplexNumber[T]] = paren(element) ~ ("+" ~> paren(element) <~ "i") ^^ {
-        case real ~ imag => new ComplexNumber(real, imag)(ring)
+    private def addition[T <: RingElement[T]](element : Parser[T], ring : Ring[T]) : Parser[(T, T)] = paren(element) ~ ("+" ~> paren(element) <~ "i") ^^ {
+        case real ~ imag => (real, imag)
     }
     
-    private def subtraction[T <: RingElement[T]](element : Parser[T], ring : Ring[T]) : Parser[ComplexNumber[T]] = paren(element) ~ ("-" ~> paren(element) <~ "i") ^^ {
-        case real ~ imag => new ComplexNumber(real, -imag)(ring)
+    private def subtraction[T <: RingElement[T]](element : Parser[T], ring : Ring[T]) : Parser[(T, T)] = paren(element) ~ ("-" ~> paren(element) <~ "i") ^^ {
+        case real ~ imag => (real, -imag)
     }
     
-    private def real[T <: RingElement[T]](element : Parser[T], ring : Ring[T]) : Parser[ComplexNumber[T]] = paren(element) ^^ {
-        case real => new ComplexNumber(real, ring.zero)(ring)
+    private def real[T <: RingElement[T]](element : Parser[T], ring : Ring[T]) : Parser[(T, T)] = paren(element) ^^ {
+        case real => (real, ring.zero)
     }
     
-    private def imaginary[T <: RingElement[T]](element : Parser[T], ring : Ring[T]) : Parser[ComplexNumber[T]] = paren(element) <~ "i" ^^ {
-        case imag => new ComplexNumber(ring.zero, imag)(ring)
+    private def imaginary[T <: RingElement[T]](element : Parser[T], ring : Ring[T]) : Parser[(T, T)] = paren(element) <~ "i" ^^ {
+        case imag => (ring.zero, imag)
     }
     
 }
