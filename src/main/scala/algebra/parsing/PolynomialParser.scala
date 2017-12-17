9fbd02a098d33f436ae85591c3d15c0bfe138196
Torstein Vik <torsteinv64@gmail.com>
2017-12-17 22:11:18 +0100

    Added apply stub to Polynomial parser

--- src/main/scala/algebra/parsing/PolynomialParser.scala
+++ src/main/scala/algebra/parsing/PolynomialParser.scala
@@ -6,5 +6,8 @@ import io.github.torsteinvik.zetatypes.algebra.structures._
 import io.github.torsteinvik.zetatypes.algebra._
 
 object PolynomialParser {
+    import AlgebraicParser._
+    def apply[T <: RingElement[T]](element : Parser[T], ring : Ring[T]) : Parser[Polynomial[T]] = ???
+    
     
 }
