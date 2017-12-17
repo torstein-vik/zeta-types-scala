482a33e1710d3ff06d14efcd71cc266de256500c
Torstein Vik <torsteinv64@gmail.com>
2017-12-17 14:49:42 +0100

    Added polynomials to structures package doc

--- src/main/scala/algebra/structures/package.scala
+++ src/main/scala/algebra/structures/package.scala
@@ -1,7 +1,7 @@
 package io.github.torsteinvik.zetatypes.algebra
 
 /** Provides instances of various algerbraic structures, such as [[ComplexNumber]]
- *  , [[Fractions]] (for any ring), and the [[Integers]]. Also has the 
+ *  , [[Fractions]] (for any ring), [[Polynomial]] (for any ring), and the [[Integers]]. Also has the 
  *  subpackage [[DSL]], which houses various implicit converters for simplifying
  *  syntax. Also provides type synonyms [[Rational]] = [[Fraction]] of [[Integer]],
  *  and [[Complex]] = [[ComplexNumber]] of [[Rational]]
