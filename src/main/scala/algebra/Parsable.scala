9d688e1abeb6cc5ac0eae451ff8741ebb0fd2cd0
Torstein Vik <torsteinv64@gmail.com>
2017-12-17 02:25:35 +0100

    Added doc for ParserException

--- src/main/scala/algebra/Parsable.scala
+++ src/main/scala/algebra/Parsable.scala
@@ -3,5 +3,10 @@ package io.github.torsteinvik.zetatypes.algebra
 import scala.util.parsing.combinator._
 import scala.util.parsing.input._
 
+
+/** Exception coming from lexing the input
+ *  @param msg message of the exception
+ *  @param pos position in the input
+ */
 case class ParserException(msg : String, pos : Position) extends 
     Exception("Parsing error at line " + pos.line + " column " + pos.column + "\n" + pos.longString)
