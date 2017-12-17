67e8cbe45651067c905de5f428ba9436ea18f769
Torstein Vik <torsteinv64@gmail.com>
2017-12-17 15:30:03 +0100

    Added integer parsing test

--- src/test/scala/ParserTest.scala
+++ src/test/scala/ParserTest.scala
@@ -8,5 +8,16 @@ import io.github.torsteinvik.zetatypes.algebra.parsing._
 
 class ParserTest extends FunSuite {
     
+    test ("integer test") {
+        assert(parse[Integer]("12") === Integer(12))
+        assert(parse[Integer]("1232423423423253245235") === Integer(BigInt("1232423423423253245235")))
+        assert(parse[Integer]("-7214172481") === Integer(-BigInt("7214172481")))
+        
+        assertThrows[ParserException]{ parse[Integer]("342342 43242") }
+        assertThrows[ParserException]{ parse[Integer]("- 34234243242") }
+        assertThrows[ParserException]{ parse[Integer]("hello") }
+        
+    }
+        
     
 }
