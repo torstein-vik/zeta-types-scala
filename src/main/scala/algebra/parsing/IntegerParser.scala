f1c4d1c8731ef026006d5bbcdd227413b2809c7b
Torstein Vik <torsteinv64@gmail.com>
2017-12-17 15:13:31 +0100

    Added file which parses an algebraic integer

---
+++ src/main/scala/algebra/parsing/IntegerParser.scala
@@ -0,0 +1,8 @@
+package io.github.torsteinvik.zetatypes.algebra.parsing
+
+import scala.util.parsing.combinator._
+
+import io.github.torsteinvik.zetatypes.algebra.structures._
+
+object IntegerParser{
+}
