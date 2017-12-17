88e3f864e2b374561ae63f096b33dfc557c33e83
Torstein Vik <torsteinv64@gmail.com>
2017-12-17 16:10:48 +0100

    Added file for fraction parsing

---
+++ src/main/scala/algebra/parsing/FractionParser.scala
@@ -0,0 +1,9 @@
+package io.github.torsteinvik.zetatypes.algebra.parsing
+
+import scala.util.parsing.combinator._
+
+import io.github.torsteinvik.zetatypes.algebra.structures._
+import io.github.torsteinvik.zetatypes.algebra._
+
+object FractionParser {
+}
