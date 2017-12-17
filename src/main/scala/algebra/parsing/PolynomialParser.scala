0ac7fff43f154d2584a67bc29f1b6759e0db0537
Torstein Vik <torsteinv64@gmail.com>
2017-12-17 22:11:36 +0100

    Added doc to PolyonmialParser

--- src/main/scala/algebra/parsing/PolynomialParser.scala
+++ src/main/scala/algebra/parsing/PolynomialParser.scala
@@ -5,8 +5,10 @@ import scala.util.parsing.combinator._
 import io.github.torsteinvik.zetatypes.algebra.structures._
 import io.github.torsteinvik.zetatypes.algebra._
 
+/** Parses an algebraic [[io.github.torsteinvik.zetatypes.algebra.structures.Polynomial]] from an input String */
 object PolynomialParser {
     import AlgebraicParser._
+    /** Parses an algebraic [[io.github.torsteinvik.zetatypes.algebra.structures.Polynomial]] from an input String */
     def apply[T <: RingElement[T]](element : Parser[T], ring : Ring[T]) : Parser[Polynomial[T]] = ???
     
     
