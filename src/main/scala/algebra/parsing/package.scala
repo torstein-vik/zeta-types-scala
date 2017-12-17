32f22af701251b9860c354a6301f3a471241f2d6
Torstein Vik <torsteinv64@gmail.com>
2017-12-17 16:11:26 +0100

    Added fraction parser to package

--- src/main/scala/algebra/parsing/package.scala
+++ src/main/scala/algebra/parsing/package.scala
@@ -1,6 +1,7 @@
 package io.github.torsteinvik.zetatypes.algebra
 
 import io.github.torsteinvik.zetatypes.algebra.structures._
+import io.github.torsteinvik.zetatypes.algebra._
 
 /** Provides implicit parsers of various algerbraic structures, such as [[ComplexNumber]]
  *  , [[Fractions]] (for any ring), and the [[Integers]]. Also has the 
@@ -14,7 +15,7 @@ package object parsing {
     def parse[T <: AlgebraicElement] (s : String)(implicit parser : AlgebraicParser.Parser[T]) = AlgebraicParser.parse(s)(parser)
     
     implicit def integerparser : AlgebraicParser.Parser[Integer] = IntegerParser.apply
-    implicit def fractionparser[T <: RingElement[T]] : AlgebraicParser.Parser[Fraction[T]] = ???
+    implicit def fractionparser[T <: RingElement[T]](implicit element : AlgebraicParser.Parser[T], ring : Ring[T]) : AlgebraicParser.Parser[Fraction[T]] = FractionParser.apply(element, ring)
     implicit def complexparser[T <: RingElement[T]] : AlgebraicParser.Parser[ComplexNumber[T]] = ???
     
 }
