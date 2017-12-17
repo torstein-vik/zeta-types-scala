0f92dbf7c402df08ead9146f65d25f8faaaf9430
Torstein Vik <torsteinv64@gmail.com>
2017-12-17 23:14:42 +0100

    Properly grouped polynomial term parsers

--- src/main/scala/algebra/parsing/PolynomialParser.scala
+++ src/main/scala/algebra/parsing/PolynomialParser.scala
@@ -12,10 +12,8 @@ object PolynomialParser {
     def apply[T <: RingElement[T]](element : Parser[T], ring : Ring[T]) : Parser[Polynomial[T]] = rep1sep(term(paren(element), ring), "+") ^^ (new Polynomial(_)(ring))
     
     private def term[T <: RingElement[T]](element : Parser[T], ring : Ring[T]) : Parser[(T, Int)] = 
-        constant(element, ring) | 
-        nocoefflinear(ring) | 
-        nocoeffpower(ring) | 
-        linear(element, ring) | 
+        (linear(element, ring) | constant(element, ring)) |
+        (nocoefflinear(ring) ||| nocoeffpower(ring)) |||
         power(element, ring)
         
     private def constant[T <: RingElement[T]](element : Parser[T], ring : Ring[T]) : Parser[(T, Int)] = element ^^ ((_, 0))
