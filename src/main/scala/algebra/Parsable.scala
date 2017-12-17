50dd056bf77f9a09d5f81f86fb3653e2ce881a9f
Torstein Vik <torsteinv64@gmail.com>
2017-12-17 02:25:21 +0100

    Added case class for parsing exceptions

--- src/main/scala/algebra/Parsable.scala
+++ src/main/scala/algebra/Parsable.scala
@@ -1,2 +1,7 @@
 package io.github.torsteinvik.zetatypes.algebra
 
+import scala.util.parsing.combinator._
+import scala.util.parsing.input._
+
+case class ParserException(msg : String, pos : Position) extends 
+    Exception("Parsing error at line " + pos.line + " column " + pos.column + "\n" + pos.longString)
