1d18cac338eee4a8fa7b10da7e9dc85a0353299b
Torstein Vik <torsteinv64@gmail.com>
2017-12-17 16:11:00 +0100

    Added fraction parser

--- src/main/scala/algebra/parsing/FractionParser.scala
+++ src/main/scala/algebra/parsing/FractionParser.scala
@@ -6,4 +6,9 @@ import io.github.torsteinvik.zetatypes.algebra.structures._
 import io.github.torsteinvik.zetatypes.algebra._
 
 object FractionParser {
+    import AlgebraicParser._
+    def apply[T <: RingElement[T]](element : Parser[T], ring : Ring[T]) : Parser[Fraction[T]] = fraction(element, ring) | (element ^^ (new Fraction(_, ring.one)(ring)))
+    private def fraction[T <: RingElement[T]](element : Parser[T], ring : Ring[T]) : Parser[Fraction[T]] = paren(element) ~ ("/" ~> paren(element)) ^^ {
+        case numerator ~ denominator => new Fraction(numerator, denominator)(ring)
+    }
 }
