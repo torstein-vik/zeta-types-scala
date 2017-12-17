e8422a3f87443e9b2336183fdf5ce7e6dc1d6ca7
Torstein Vik <torsteinv64@gmail.com>
2017-12-17 02:27:38 +0100

    Added doc to Parsable

--- src/main/scala/algebra/Parsable.scala
+++ src/main/scala/algebra/Parsable.scala
@@ -3,6 +3,9 @@ package io.github.torsteinvik.zetatypes.algebra
 import scala.util.parsing.combinator._
 import scala.util.parsing.input._
 
+/** Some [[AlgebraicStructure]] with elements that are parsable (from Strings) 
+ *  @tparam T The type of [[AlgebraicElement]] that is parsed from a string
+ */
 trait Parsable[T <: AlgebraicElement] extends AlgebraicStructure[T] with RegexParsers {
     def parser : Parser[T]
 }
