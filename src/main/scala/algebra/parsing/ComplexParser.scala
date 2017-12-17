116a83a109436c345e96752c3eed4c9fe280054e
Torstein Vik <torsteinv64@gmail.com>
2017-12-17 21:34:32 +0100

    Added various complex-number parsers

--- src/main/scala/algebra/parsing/ComplexParser.scala
+++ src/main/scala/algebra/parsing/ComplexParser.scala
@@ -6,4 +6,20 @@ import io.github.torsteinvik.zetatypes.algebra.structures._
 import io.github.torsteinvik.zetatypes.algebra._
 
 object ComplexParser {
+    private def addition[T <: RingElement[T]](element : Parser[T], ring : Ring[T]) : Parser[ComplexNumber[T]] = paren(element) ~ ("+" ~> paren(element) <~ "i") ^^ {
+        case real ~ imag => new ComplexNumber(real, imag)(ring)
+    }
+    
+    private def subtraction[T <: RingElement[T]](element : Parser[T], ring : Ring[T]) : Parser[ComplexNumber[T]] = paren(element) ~ ("-" ~> paren(element) <~ "i") ^^ {
+        case real ~ imag => new ComplexNumber(real, -imag)(ring)
+    }
+    
+    private def real[T <: RingElement[T]](element : Parser[T], ring : Ring[T]) : Parser[ComplexNumber[T]] = paren(element) ^^ {
+        case real => new ComplexNumber(real, ring.zero)(ring)
+    }
+    
+    private def imaginary[T <: RingElement[T]](element : Parser[T], ring : Ring[T]) : Parser[ComplexNumber[T]] = paren(element) <~ "i" ^^ {
+        case imag => new ComplexNumber(ring.zero, imag)(ring)
+    }
+    
 }
