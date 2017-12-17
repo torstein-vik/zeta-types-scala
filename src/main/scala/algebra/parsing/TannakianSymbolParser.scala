341931113dbaecd5922f3cb38a7310bd9e622b6c
Torstein Vik <torsteinv64@gmail.com>
2017-12-18 00:08:22 +0100

    Added doc to TSParser

--- src/main/scala/algebra/parsing/TannakianSymbolParser.scala
+++ src/main/scala/algebra/parsing/TannakianSymbolParser.scala
@@ -6,8 +6,10 @@ import io.github.torsteinvik.zetatypes.algebra._
 import io.github.torsteinvik.zetatypes.tannakiansymbols._
 import io.github.torsteinvik.zetatypes.tannakiansymbols.DSL._
 
+/** Parses an algebraic [[io.github.torsteinvik.zetatypes.tannakiansymbols.TannakianSymbol]] from an input String */
 object TannakianSymbolParser {
     import AlgebraicParser._
+    /** Parses an algebraic [[io.github.torsteinvik.zetatypes.tannakiansymbols.TannakianSymbol]] from an input String */
     def apply[T <: MonoidElement](element : Parser[T], monoid : Monoid[T]) : Parser[TannakianSymbol[T]] = ???
     
 }
