8fdc1bf32f0caadb7b7ab981d855acaf79e85e59
Torstein Vik <torsteinv64@gmail.com>
2017-12-17 14:52:53 +0100

    Restructuring parsing, added new package for parsing

---
+++ src/main/scala/algebra/parsing/package.scala
@@ -0,0 +1,12 @@
+package io.github.torsteinvik.zetatypes.algebra
+
+/** Provides implicit parsers of various algerbraic structures, such as [[ComplexNumber]]
+ *  , [[Fractions]] (for any ring), and the [[Integers]]. Also has the 
+ *  subpackage [[DSL]], which houses various implicit converters for simplifying
+ *  syntax. Also provides type synonyms [[Rational]] = [[Fraction]] of [[Integer]],
+ *  and [[Complex]] = [[ComplexNumber]] of [[Rational]]
+ *  @author Torstein Vik
+ */
+package object parsing {
+    
+}
