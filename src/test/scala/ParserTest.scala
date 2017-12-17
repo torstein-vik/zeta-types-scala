10dab3d48766247a2cfbfae1816436d4d77ea000
Torstein Vik <torsteinv64@gmail.com>
2017-12-17 15:29:46 +0100

    Added file and class for parser testing

---
+++ src/test/scala/ParserTest.scala
@@ -0,0 +1,12 @@
+package io.github.torsteinvik.zetatypes.test
+
+import org.scalatest.FunSuite
+
+import io.github.torsteinvik.zetatypes.algebra.structures._
+import io.github.torsteinvik.zetatypes.algebra.structures.DSL._
+import io.github.torsteinvik.zetatypes.algebra.parsing._
+
+class ParserTest extends FunSuite {
+    
+    
+}
