dad6eee180be834da3d8e36d34fc601e520acf89
Torstein Vik <torsteinv64@gmail.com>
2017-12-17 21:33:27 +0100

    Fixed AlgebraicParser.parse doc

--- src/main/scala/algebra/parsing/Parsing.scala
+++ src/main/scala/algebra/parsing/Parsing.scala
@@ -6,7 +6,7 @@ import scala.util.parsing.input._
 /** Global parsing object */
 object AlgebraicParser extends RegexParsers {
     
-    /** Returns [[parser]] applied to the input string */
+    /** Returns some parser of T applied to the input string */
     def parse[T] (str : String)(implicit parser : Parser[T]) : T = parseAll(parser, str) match {
         case Success(result, _) => result
         case Failure(msg, next) => throw ParserException(msg, next.pos)
