d195b86ce06331a79cfc70d902dcfd836a6031f8
Torstein Vik <torsteinv64@gmail.com>
2017-12-17 15:35:01 +0100

    Added implicit parser to parsing package

--- src/main/scala/algebra/parsing/package.scala
+++ src/main/scala/algebra/parsing/package.scala
@@ -1,5 +1,7 @@
 package io.github.torsteinvik.zetatypes.algebra
 
+import io.github.torsteinvik.zetatypes.algebra.structures._
+
 /** Provides implicit parsers of various algerbraic structures, such as [[ComplexNumber]]
  *  , [[Fractions]] (for any ring), and the [[Integers]]. Also has the 
  *  subpackage [[DSL]], which houses various implicit converters for simplifying
@@ -10,5 +12,9 @@ package io.github.torsteinvik.zetatypes.algebra
 package object parsing {
     /** Synonym for [[AlgebraicParser.parse]] */
     def parse[T <: AlgebraicElement] (s : String)(implicit parser : AlgebraicParser.Parser[T]) = AlgebraicParser.parse(s)(parser)
+    
+    implicit def integerparser : AlgebraicParser.Parser[Integer] = ???
+    implicit def fractionparser[T <: RingElement[T]] : AlgebraicParser.Parser[Fraction[T]] = ???
+    implicit def complexparser[T <: RingElement[T]] : AlgebraicParser.Parser[ComplexNumber[T]] = ???
     
 }
