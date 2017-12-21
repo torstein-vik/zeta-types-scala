303316ee3b9a2a9c52b781f16e0c82e7f481c981
Torstein Vik <torsteinv64@gmail.com>
2017-12-21 18:38:41 +0100

    Added package doc

--- src/main/scala/algebra/morphisms/package.scala
+++ src/main/scala/algebra/morphisms/package.scala
@@ -1,3 +1,7 @@
 package io.github.torsteinvik.zetatypes.algebra
 
+/** Provides traits for various types of morphisms between algebraic structures.
+ *  Also provides implicit instances of some morphisms, and functionality for applying them.
+ *  @author Torstein Vik
+ */
 package object morphisms 
