c0783f1bb15c709932b7f70a85e0f31ca118bf67
Torstein Vik <torsteinv64@gmail.com>
2017-12-23 22:43:22 +0100

    Implemented composition apply

--- src/main/scala/algebra/morphisms/Composition.scala
+++ src/main/scala/algebra/morphisms/Composition.scala
@@ -4,6 +4,8 @@ import io.github.torsteinvik.zetatypes.algebra._
 
 private[morphisms] class Composition[A <: AlgebraicElement, B <: AlgebraicElement, C <: AlgebraicElement]
     (f : Homomorphism[B, C], g : Homomorphism[A, B]) extends Homomorphism[A, C] {
+    def apply (x : A) = f(g(x))
 }
+
 object Composition {
 }
