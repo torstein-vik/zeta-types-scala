b11199e4b111f1e7aaa33ecd35f1d68c9d74506f
Torstein Vik <torsteinv64@gmail.com>
2017-12-23 22:34:52 +0100

    Added Homomorphism trait

--- src/main/scala/algebra/morphisms/Homomorphism.scala
+++ src/main/scala/algebra/morphisms/Homomorphism.scala
@@ -1,2 +1,7 @@
 package io.github.torsteinvik.zetatypes.algebra.morphisms
 
+import io.github.torsteinvik.zetatypes.algebra._
+
+trait Homomorphism[-A <: AlgebraicElement, +B <: AlgebraicElement] extends (A => B) {
+}
+
