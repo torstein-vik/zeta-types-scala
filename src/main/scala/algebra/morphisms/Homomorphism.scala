b7b5f23db7207ed83c4b6d5d76a019ac966cbc61
Torstein Vik <torsteinv64@gmail.com>
2017-12-23 22:35:14 +0100

    Added apply method to homomorphism trait

--- src/main/scala/algebra/morphisms/Homomorphism.scala
+++ src/main/scala/algebra/morphisms/Homomorphism.scala
@@ -3,5 +3,6 @@ package io.github.torsteinvik.zetatypes.algebra.morphisms
 import io.github.torsteinvik.zetatypes.algebra._
 
 trait Homomorphism[-A <: AlgebraicElement, +B <: AlgebraicElement] extends (A => B) {
+    def apply(input : A) : B
 }
 
