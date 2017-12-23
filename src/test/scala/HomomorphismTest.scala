ede7becc4e03ac64d9c9ba4738708ffff0cceb95
Torstein Vik <torsteinv64@gmail.com>
2017-12-23 22:54:19 +0100

    Added test for homomorphism DSL notation

--- src/test/scala/HomomorphismTest.scala
+++ src/test/scala/HomomorphismTest.scala
@@ -7,6 +7,15 @@ import io.github.torsteinvik.zetatypes.algebra.morphisms._
 class HomomorphismTest extends FunSuite {
     
     {// Type synonym test
+        import io.github.torsteinvik.zetatypes.algebra.morphisms.DSL._
+        import io.github.torsteinvik.zetatypes.algebra.structures.Integer
+        
+        test ("Homomorphism notation") {
+            assertCompiles("val f : Integer --> Integer = identity")
+            assertCompiles("val f : Integer ^-> Integer = identity")
+            assertCompiles("val f : Integer ->> Integer = identity")
+            assertCompiles("val f : Integer ~~> Integer = identity")
+        }
         
     }
     
