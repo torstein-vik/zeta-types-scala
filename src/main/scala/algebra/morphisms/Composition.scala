2864c9d72a922c55861d9292743b0eb05793b5b2
Torstein Vik <torsteinv64@gmail.com>
2017-12-23 22:43:03 +0100

    Added private class for composition morphism

--- src/main/scala/algebra/morphisms/Composition.scala
+++ src/main/scala/algebra/morphisms/Composition.scala
@@ -2,5 +2,8 @@ package io.github.torsteinvik.zetatypes.algebra.morphisms
 
 import io.github.torsteinvik.zetatypes.algebra._
 
+private[morphisms] class Composition[A <: AlgebraicElement, B <: AlgebraicElement, C <: AlgebraicElement]
+    (f : Homomorphism[B, C], g : Homomorphism[A, B]) extends Homomorphism[A, C] {
+}
 object Composition {
 }
