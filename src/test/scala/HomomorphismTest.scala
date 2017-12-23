af07327255c9436ac3f8cb4bd08c49946b9ba09a
Torstein Vik <torsteinv64@gmail.com>
2017-12-23 22:53:21 +0100

    Added class for homomorphism test suite

--- src/test/scala/HomomorphismTest.scala
+++ src/test/scala/HomomorphismTest.scala
@@ -1,2 +1,9 @@
 package io.github.torsteinvik.zetatypes.test
 
+import org.scalatest.FunSuite
+
+import io.github.torsteinvik.zetatypes.algebra.morphisms._
+
+class HomomorphismTest extends FunSuite {
+    
+}
