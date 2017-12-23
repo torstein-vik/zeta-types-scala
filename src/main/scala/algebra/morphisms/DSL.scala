28b4625da823337df778c1ad0ae56354a21d42d0
Torstein Vik <torsteinv64@gmail.com>
2017-12-23 22:49:50 +0100

    Added dummy class to package DSL

--- src/main/scala/algebra/morphisms/DSL.scala
+++ src/main/scala/algebra/morphisms/DSL.scala
@@ -3,4 +3,7 @@ package io.github.torsteinvik.zetatypes.algebra.morphisms
 package object DSL {
 }
 package DSL {
+    
+    /** Dummy class so that scaladoc shows DSL */
+    protected class DummyClass
 }
