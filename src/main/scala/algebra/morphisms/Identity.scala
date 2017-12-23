0a032334344064b785b2ce84c3c90190f1714c95
Torstein Vik <torsteinv64@gmail.com>
2017-12-23 22:39:26 +0100

    Added doc to identity morphism

--- src/main/scala/algebra/morphisms/Identity.scala
+++ src/main/scala/algebra/morphisms/Identity.scala
@@ -1,6 +1,10 @@
 package io.github.torsteinvik.zetatypes.algebra.morphisms
 
 import io.github.torsteinvik.zetatypes.algebra._
+
+/** Identity morphism on A
+ *  @tparam A The [[io.github.torsteinvik.zetatypes.algebra.AlgebraicElement]] this identity morphism maps
+ */
 case class Identity[A <: AlgebraicElement]() extends Isomorphism[A, A] {
     def apply (x : A) = x
     def inverse = Identity[A]()
