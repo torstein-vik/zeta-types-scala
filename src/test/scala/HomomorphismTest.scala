26bff08330b59e0c61759edbcb5e280716b57a22
Torstein Vik <torsteinv64@gmail.com>
2017-12-23 22:54:01 +0100

    Added some env's for different homomorphism tests

--- src/test/scala/HomomorphismTest.scala
+++ src/test/scala/HomomorphismTest.scala
@@ -6,4 +6,19 @@ import io.github.torsteinvik.zetatypes.algebra.morphisms._
 
 class HomomorphismTest extends FunSuite {
     
+    {// Type synonym test
+        
+    }
+    
+    {// Composition injectivity and surjectivity test
+        
+    }
+    
+    {// Composition application test
+        
+    }
+    
+    {// Identitity application test
+            
+    }
 }
