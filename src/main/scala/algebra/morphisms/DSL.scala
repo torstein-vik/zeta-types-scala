56e931bb8244a459e815c06b74a472df34972404
Torstein Vik <torsteinv64@gmail.com>
2017-12-23 22:50:15 +0100

    Added type synonyms for homomorphisms

--- src/main/scala/algebra/morphisms/DSL.scala
+++ src/main/scala/algebra/morphisms/DSL.scala
@@ -1,7 +1,15 @@
 package io.github.torsteinvik.zetatypes.algebra.morphisms
 
+import io.github.torsteinvik.zetatypes.algebra._
+
 package object DSL {
+    type -->[A <: AlgebraicElement, B <: AlgebraicElement] = Homomorphism[A, B]
+    type ^->[A <: AlgebraicElement, B <: AlgebraicElement] = Epimorphism[A, B]
+    type ->>[A <: AlgebraicElement, B <: AlgebraicElement] = Monomorphism[A, B]
+    type ~~>[A <: AlgebraicElement, B <: AlgebraicElement] = Isomorphism[A, B]
+    
 }
+
 package DSL {
     
     /** Dummy class so that scaladoc shows DSL */
