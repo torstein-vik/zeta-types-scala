2076e0c18e7b0bb064c66c25d7beec4a544f08f6
Torstein Vik <torsteinv64@gmail.com>
2017-12-23 22:43:59 +0100

    Added composition object apply mixing in epi and mono traits when given

--- src/main/scala/algebra/morphisms/Composition.scala
+++ src/main/scala/algebra/morphisms/Composition.scala
@@ -8,4 +8,10 @@ private[morphisms] class Composition[A <: AlgebraicElement, B <: AlgebraicElemen
 }
 
 object Composition {
+    def apply[A <: AlgebraicElement, B <: AlgebraicElement, C <: AlgebraicElement]
+             (f : Homomorphism[B, C], g : Homomorphism[A, B]) :      Composition[A, B, C] = (f, g) match {
+        case (f : Monomorphism[B, C], g : Monomorphism[A, B]) => new Composition[A, B, C]   (f, g) with Monomorphism[A, C]
+        case (f : Epimorphism[B, C],  g : Epimorphism[A, B])  => new Composition[A, B, C]   (f, g) with Epimorphism[A, C]
+        case (f, g)                                           => new Composition[A, B, C]   (f, g)
+    }
 }
