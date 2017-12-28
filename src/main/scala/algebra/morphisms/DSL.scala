af23b6d154b1a7f0971cd4e25798e3356d449a3a
Torstein Vik <torsteinv64@gmail.com>
2017-12-28 04:33:25 +0100

    Fixed bad type synonyms

--- src/main/scala/algebra/morphisms/DSL.scala
+++ src/main/scala/algebra/morphisms/DSL.scala
@@ -8,10 +8,10 @@ import io.github.torsteinvik.zetatypes.algebra._
 package object DSL {
     /** Synonym for [[Homomorphism]]*/
     type -->[A <: AlgebraicElement, B <: AlgebraicElement] = Homomorphism[A, B]
-    /** Synonym for [[Epimorphism]]*/
-    type ^->[A <: AlgebraicElement, B <: AlgebraicElement] = Epimorphism[A, B]
     /** Synonym for [[Monomorphism]]*/
-    type ->>[A <: AlgebraicElement, B <: AlgebraicElement] = Monomorphism[A, B]
+    type ^->[A <: AlgebraicElement, B <: AlgebraicElement] = Monomorphism[A, B]
+    /** Synonym for [[Epimorphism]]*/
+    type ->>[A <: AlgebraicElement, B <: AlgebraicElement] = Epimorphism[A, B]
     /** Synonym for [[Isomorphism]]*/
     type ~~>[A <: AlgebraicElement, B <: AlgebraicElement] = Isomorphism[A, B]
     
