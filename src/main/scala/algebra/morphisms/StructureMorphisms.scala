496d1dbc7317861d0c69c0252cc2b92af39f58eb
Torstein Vik <torsteinv64@gmail.com>
2017-12-28 04:34:06 +0100

    Added fraction inclusion morphism

--- src/main/scala/algebra/morphisms/StructureMorphisms.scala
+++ src/main/scala/algebra/morphisms/StructureMorphisms.scala
@@ -6,5 +6,8 @@ import io.github.torsteinvik.zetatypes.algebra.structures._
 
 /** Object with various kinds of structural morphisms */
 object StructureMorphisms {
+    implicit def fractionInclusion[E <: RingElement[E]](implicit ring : Ring[E]) : (E ^-> Fraction[E]) = new Monomorphism[E, Fraction[E]]{
+        def apply(x : E) = new Fraction(x, ring.one)(ring)
+    }
     
 }
