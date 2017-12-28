b6dc8e8536f3b4152ab79d33e88529f2bd112fca
Torstein Vik <torsteinv64@gmail.com>
2017-12-28 04:33:56 +0100

    Added files for structure morphisms

---
+++ src/main/scala/algebra/morphisms/StructureMorphisms.scala
@@ -0,0 +1,10 @@
+package io.github.torsteinvik.zetatypes.algebra.morphisms
+
+import io.github.torsteinvik.zetatypes.algebra._
+import io.github.torsteinvik.zetatypes.algebra.morphisms.DSL._
+import io.github.torsteinvik.zetatypes.algebra.structures._
+
+/** Object with various kinds of structural morphisms */
+object StructureMorphisms {
+    
+}
