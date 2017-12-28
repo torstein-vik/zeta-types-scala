49c210d9b1c1b5b1ffac2d36433a9edd352ddc9f
Torstein Vik <torsteinv64@gmail.com>
2017-12-28 02:13:53 +0100

    Added doc to Homomorphism "of" and "and"

--- src/main/scala/algebra/morphisms/Homomorphism.scala
+++ src/main/scala/algebra/morphisms/Homomorphism.scala
@@ -9,7 +9,16 @@ import io.github.torsteinvik.zetatypes.algebra._
 trait Homomorphism[A <: AlgebraicElement, B <: AlgebraicElement] extends (A => B) {
     /** Apply this homomorphism to some input element from the domain */
     def apply(input : A) : B
+    
+    /** Compose this morphism with some other morphism 
+     *  @tparam C the input type of the final morphism
+     *  @param f the morphism to compose with
+     */
     final def of [C <: AlgebraicElement](f : Homomorphism[C, A]) = Composition(this, f)
+    /** Precompose this morphism with some other morphism 
+     *  @tparam C the output type of the final morphism
+     *  @param f the morphism to precompose with
+     */
     final def and [C <: AlgebraicElement](f : Homomorphism[B, C]) = Composition(f, this)
 }
 
