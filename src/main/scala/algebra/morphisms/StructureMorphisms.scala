9584610a7516564e9352557caebf3fdb310919d3
Torstein Vik <torsteinv64@gmail.com>
2017-12-28 04:34:44 +0100

    Added doc to structure morphisms

--- src/main/scala/algebra/morphisms/StructureMorphisms.scala
+++ src/main/scala/algebra/morphisms/StructureMorphisms.scala
@@ -6,14 +6,17 @@ import io.github.torsteinvik.zetatypes.algebra.structures._
 
 /** Object with various kinds of structural morphisms */
 object StructureMorphisms {
+    /** Natural inclusion of a ring into its ring of fractions */
     implicit def fractionInclusion[E <: RingElement[E]](implicit ring : Ring[E]) : (E ^-> Fraction[E]) = new Monomorphism[E, Fraction[E]]{
         def apply(x : E) = new Fraction(x, ring.one)(ring)
     }
     
+    /** Natural inclusion of a ring into its ring of complex numbers */
     implicit def complexInclusion[E <: RingElement[E]](implicit ring : Ring[E]) : (E ^-> ComplexNumber[E]) = new Monomorphism[E, ComplexNumber[E]]{
         def apply(x : E) = new ComplexNumber(x, ring.zero)(ring)
     }
     
+    /** Natural inclusion of a ring into the ring of polynomials with coefficients in that ring */
     implicit def polynomialInclusion[E <: RingElement[E]](implicit ring : Ring[E]) : (E ^-> Polynomial[E]) = new Monomorphism[E, Polynomial[E]]{
         def apply(x : E) = new Polynomial(Seq((x, 0)))(ring)
     }
