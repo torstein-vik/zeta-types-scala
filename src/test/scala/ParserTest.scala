684a0f20ea76ecb3ed1358b23c9f5393dba4d166
Torstein Vik <torsteinv64@gmail.com>
2018-01-02 18:41:03 +0100

    Removed unused imports from parser test

--- src/test/scala/ParserTest.scala
+++ src/test/scala/ParserTest.scala
@@ -2,12 +2,10 @@ package io.github.torsteinvik.zetatypes.test
 
 import org.scalatest.FunSuite
 
-import io.github.torsteinvik.zetatypes.algebra._
 import io.github.torsteinvik.zetatypes.algebra.structures._
 import io.github.torsteinvik.zetatypes.algebra.structures.DSL._
 import io.github.torsteinvik.zetatypes.algebra.parsing._
 import io.github.torsteinvik.zetatypes.tannakiansymbols._
-import io.github.torsteinvik.zetatypes.tannakiansymbols.DSL._
 
 class ParserTest extends FunSuite {
     
