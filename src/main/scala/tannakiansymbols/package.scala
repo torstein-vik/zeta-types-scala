6e330a05e9ae7eb90de3e31a296dad44437ca7d2
Torstein Vik <torsteinv64@gmail.com>
2018-01-02 19:38:06 +0100

    Fixed detail in ts package doc

--- src/main/scala/tannakiansymbols/package.scala
+++ src/main/scala/tannakiansymbols/package.scala
@@ -2,7 +2,7 @@ package io.github.torsteinvik.zetatypes
 
 /** Provides classes for working with Tannakian Symbols, and some tools for 
  *  doing this with a nice syntax. Most importantly, the [[TannakianSymbol]]
- *  class implements all computational behaviour. AlgebraicMultisets are mostly used to
+ *  class implements all computational behaviour. AlgebraicMultisets are used to
  *  to instaniate Tannakian Symbols.
  *  @author Torstein Vik
  */
