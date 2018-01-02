4bac89d56a089ed5c6a3212c786b4c2de5040c5a
Torstein Vik <torsteinv64@gmail.com>
2018-01-02 18:49:43 +0100

    Fixed ts parser with wrong number of implicit params

--- src/main/scala/algebra/parsing/TannakianSymbolParser.scala
+++ src/main/scala/algebra/parsing/TannakianSymbolParser.scala
@@ -9,7 +9,7 @@ object TannakianSymbolParser {
     import AlgebraicParser._
     /** Parses an algebraic [[io.github.torsteinvik.zetatypes.tannakiansymbols.TannakianSymbol]] from an input String */
     def apply[T <: MonoidElement](element : Parser[T], monoid : Monoid[T]) : Parser[TannakianSymbol[T]] = multiset(element) ~ ("/" ~> multiset(element)) ^^ {
-        case upstairs ~ downstairs => (ms(upstairs : _*) / ms(downstairs : _*))(monoid, implicitly)
+        case upstairs ~ downstairs => (ms(upstairs : _*) / ms(downstairs : _*))(monoid)
     }
     
     private def multiset[T <: MonoidElement](element : Parser[T]) : Parser[Seq[T]] = emptyset | ("{" ~> repsep(element, ",") <~ "}")
