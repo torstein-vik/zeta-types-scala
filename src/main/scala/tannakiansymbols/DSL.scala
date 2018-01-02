379e0730c859cdba71bf537ec3d8c08ff007b688
Torstein Vik <torsteinv64@gmail.com>
2018-01-02 18:12:18 +0100

    Added ts DSL objects for all common monoids

--- src/main/scala/tannakiansymbols/DSL.scala
+++ src/main/scala/tannakiansymbols/DSL.scala
@@ -1,6 +1,8 @@
 package io.github.torsteinvik.zetatypes.tannakiansymbols
 
 import scala.language.implicitConversions
+import io.github.torsteinvik.zetatypes.algebra.structures._
+import io.github.torsteinvik.zetatypes.algebra._
 
 /** Provides methods for creating [[Multiset]]s, which in turn can be used to create
  *  Tannakian symbols. Multisets can always be created easily, but to covert it
@@ -17,8 +19,37 @@ package object DSL {
         def Ø[T <: MonoidElement] = new Multiset[T]()
     }
     
+    object Integer {
+        /** Shorthand for new [[Multiset]]*/
+        def ms (x : Integer*) : Multiset[Integer] = new Multiset(x : _*)
+        
+        /** Shorthand for new empty [[Multiset]]*/
+        def Ø = new Multiset[Integer]()
+    }
     
+    object Rational {
+        /** Shorthand for new [[Multiset]]*/
+        def ms (x : Rational*) : Multiset[Rational] = new Multiset(x : _*)
+        
+        /** Shorthand for new empty [[Multiset]]*/
+        def Ø = new Multiset[Rational]()
+    }
     
+    object Complex {
+        /** Shorthand for new [[Multiset]]*/
+        def ms (x : Complex*) : Multiset[Complex] = new Multiset(x : _*)
+        
+        /** Shorthand for new empty [[Multiset]]*/
+        def Ø = new Multiset[Complex]()
+    }
+    
+    object ComplexPolynomial {
+        /** Shorthand for new [[Multiset]]*/
+        def ms (x : Polynomial[Complex]*) : Multiset[Polynomial[Complex]] = new Multiset(x : _*)
+        
+        /** Shorthand for new empty [[Multiset]]*/
+        def Ø = new Multiset[Polynomial[Complex]]()
+    }
 }
 
 package DSL {
