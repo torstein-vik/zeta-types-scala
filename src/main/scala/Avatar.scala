6069fa58da91536a0958466cc61a52ff17764209
Torstein Vik <torsteinv64@gmail.com>
2018-01-05 00:23:12 +0100

    Added sealed trait (gobal) Avatar

--- src/main/scala/Avatar.scala
+++ src/main/scala/Avatar.scala
@@ -1,2 +1,10 @@
 package io.github.torsteinvik.zetatypes
 
+import io.github.torsteinvik.zetatypes.numbertheory._
+
+sealed trait Avatar {
+    def localavatar (p : Prime) : LocalAvatar
+}
+
+object Avatar {
+}
