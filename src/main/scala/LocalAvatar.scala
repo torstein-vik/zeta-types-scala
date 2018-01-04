66bb3c14f47fcf773b3018d8a75cf211fcf2d1ca
Torstein Vik <torsteinv64@gmail.com>
2018-01-04 23:57:38 +0100

    Added doc to avatars

--- src/main/scala/LocalAvatar.scala
+++ src/main/scala/LocalAvatar.scala
@@ -6,12 +6,16 @@ sealed trait LocalAvatar
 /** Contains the only legal zeta-type local avatars */
 object LocalAvatar {
     
+    /** [[LocalAvatar!]] for representation using a Tannakian symbol */
     case object TS  extends LocalAvatar
     
+    /** [[LocalAvatar!]] for representation using a rational expression for the local zeta-funcation */
     case object RationalLZF extends LocalAvatar
     
+    /** [[LocalAvatar!]] for representation using Bell coefficients */
     case object BellCoeffs extends LocalAvatar
     
+    /** [[LocalAvatar!]] for representation using point counts */
     case object PointCounts extends LocalAvatar
 
 }
