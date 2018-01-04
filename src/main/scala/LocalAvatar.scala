efbf66ad2afe4c1abadee123231edd54e8e3fcf4
Torstein Vik <torsteinv64@gmail.com>
2018-01-04 23:56:36 +0100

    Added sealed trait for lzt avatar

--- src/main/scala/LocalAvatar.scala
+++ src/main/scala/LocalAvatar.scala
@@ -1,2 +1,10 @@
 package io.github.torsteinvik.zetatypes
 
+/** An avatar for the local data of a zeta-type */
+sealed trait LocalAvatar
+
+/** Contains the only legal zeta-type local avatars */
+object LocalAvatar {
+    
+
+}
