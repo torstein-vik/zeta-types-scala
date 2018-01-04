69a319cf0e4500ba4760aa2b6d3aff6f1bb69cf0
Torstein Vik <torsteinv64@gmail.com>
2018-01-05 00:23:48 +0100

    Added 4 main gobal avatars

--- src/main/scala/Avatar.scala
+++ src/main/scala/Avatar.scala
@@ -7,4 +7,20 @@ sealed trait Avatar {
 }
 
 object Avatar {
+    case object TS extends Avatar {
+        def localavatar(p : Prime) = LocalAvatar.TS
+    }
+    
+    case object RationalLZF extends Avatar {
+        def localavatar(p : Prime) = LocalAvatar.RationalLZF
+    }
+    
+    case object BellCoeffs extends Avatar {
+        def localavatar(p : Prime) = LocalAvatar.BellCoeffs
+    }
+    
+    case object PointCounts extends Avatar {
+        def localavatar(p : Prime) = LocalAvatar.PointCounts
+    }
+
 }
