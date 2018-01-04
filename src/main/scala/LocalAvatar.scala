3e263573432e718b59006180a66402771ef05f6d
Torstein Vik <torsteinv64@gmail.com>
2018-01-04 23:57:31 +0100

    Added main 4 avatars

--- src/main/scala/LocalAvatar.scala
+++ src/main/scala/LocalAvatar.scala
@@ -6,5 +6,12 @@ sealed trait LocalAvatar
 /** Contains the only legal zeta-type local avatars */
 object LocalAvatar {
     
+    case object TS  extends LocalAvatar
+    
+    case object RationalLZF extends LocalAvatar
+    
+    case object BellCoeffs extends LocalAvatar
+    
+    case object PointCounts extends LocalAvatar
 
 }
