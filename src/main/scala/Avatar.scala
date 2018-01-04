41fb159c93f0c2e603acaa28f461317324fb17ef
Torstein Vik <torsteinv64@gmail.com>
2018-01-05 00:24:03 +0100

    Added doc to all main avatars

--- src/main/scala/Avatar.scala
+++ src/main/scala/Avatar.scala
@@ -7,18 +7,23 @@ sealed trait Avatar {
 }
 
 object Avatar {
+    
+    /** [[Avatar!]] for representation using Tannakian symbols */
     case object TS extends Avatar {
         def localavatar(p : Prime) = LocalAvatar.TS
     }
     
+    /** [[Avatar!]] for representation using rational expressions for the local zeta-functions */
     case object RationalLZF extends Avatar {
         def localavatar(p : Prime) = LocalAvatar.RationalLZF
     }
     
+    /** [[Avatar!]] for representation using Bell coefficients */
     case object BellCoeffs extends Avatar {
         def localavatar(p : Prime) = LocalAvatar.BellCoeffs
     }
     
+    /** [[Avatar!]] for representation using point counts */
     case object PointCounts extends Avatar {
         def localavatar(p : Prime) = LocalAvatar.PointCounts
     }
