1ae0ff09afa51ae518de42e20f4bbb1df0977823
Torstein Vik <torsteinv64@gmail.com>
2018-01-06 18:18:15 +0100

    Added transform trait

--- src/main/scala/local/transforms/Transform.scala
+++ src/main/scala/local/transforms/Transform.scala
@@ -1,2 +1,9 @@
 package io.github.torsteinvik.zetatypes.local.transforms
 
+import io.github.torsteinvik.zetatypes.local._
+import Avatar._
+
+trait Transform[A <: Avatar with Concrete, B <: Avatar with Concrete, E] {
+    type newbase
+    def apply (input : ZetaType[E, A]) : ZetaType[newbase, B]
+}
