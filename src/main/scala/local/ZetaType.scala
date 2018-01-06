17420903bfb24f8d1a49364d43086e2861aeff94
Torstein Vik <torsteinv64@gmail.com>
2018-01-06 18:18:26 +0100

    Added transforms import to ZetaType

--- src/main/scala/local/ZetaType.scala
+++ src/main/scala/local/ZetaType.scala
@@ -1,6 +1,7 @@
 package io.github.torsteinvik.zetatypes.local
 
 import Avatar._
+import transforms._
 
 abstract class ZetaType[E, A <: Avatar with Concrete] {
     def to[B <: Avatar with Concrete](implicit transform : Transform[A, B, E]) : ZetaType[transform.newbase, B] = transform(this)
