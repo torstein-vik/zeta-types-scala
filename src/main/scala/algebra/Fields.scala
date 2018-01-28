7f861ff597c175332b6c625311073ae644feca9f
Torstein Vik <torsteinv64@gmail.com>
2018-01-28 19:13:00 +0100

    Added Field trait for fields

--- src/main/scala/algebra/Fields.scala
+++ src/main/scala/algebra/Fields.scala
@@ -3,4 +3,7 @@ package io.github.torsteinvik.zetatypes.algebra
 trait FieldElement[T <: FieldElement[T]] extends GroupElement with RingElement[T] with Divisible[T] {
 }
 
+trait Field[E <: FieldElement[E]] extends Ring[E] {
+    
+}
 
