c0037fed773fe1048eb503852dc265fa0ec2077f
Torstein Vik <torsteinv64@gmail.com>
2018-01-28 19:13:42 +0100

    Added type enforcement to FieldElement canonicalRing

--- src/main/scala/algebra/Fields.scala
+++ src/main/scala/algebra/Fields.scala
@@ -1,6 +1,8 @@
 package io.github.torsteinvik.zetatypes.algebra
 
 trait FieldElement[T <: FieldElement[T]] extends GroupElement with RingElement[T] with Divisible[T] {
+    override val canonicalRing : Field[T]
+    
 }
 
 trait Field[E <: FieldElement[E]] extends Ring[E] {
