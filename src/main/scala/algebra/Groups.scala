033f1abb7c0ed2225433a51964c111594471a4a7
Torstein Vik <torsteinv64@gmail.com>
2018-01-28 21:43:22 +0100

    Fixed signatures of -Additive and MultiplicativeGroup

--- src/main/scala/algebra/Groups.scala
+++ src/main/scala/algebra/Groups.scala
@@ -24,12 +24,12 @@ trait Group[T <: GroupElement] extends PartialGroup[T] {
 }
 
 /** A [[Group]] where the elements are [[Additive]]. This allows [[invert]] to be inferred. */
-trait AdditiveGroup[T <: GroupElement with Additive[T] with Subtractive[T]] extends Group[T] with AdditiveMonoid[T] {
+trait AdditiveGroup[T <: GroupAdditive[T]] extends Group[T] with AdditiveMonoid[T] {
     def invert (x : T) = x.negation
 }
 
 /** A [[Group]] where the elements are [[Multiplicative]]. This allows [[invert]] to be inferred. */
-trait MultiplicativeGroup[T <: GroupElement with Multiplicative[T] with Divisible[T]] extends Group[T] with MultiplicativeMonoid[T] {
+trait MultiplicativeGroup[T <: GroupMultiplicative[T]] extends Group[T] with MultiplicativeMonoid[T] {
     def invert (x : T) = x.inverse
 }
 
