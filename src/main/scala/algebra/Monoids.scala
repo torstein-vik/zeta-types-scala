e6ae1ab1cb37c2add9c66f3904744cf8b696eecf
Torstein Vik <torsteinv64@gmail.com>
2018-01-28 21:38:37 +0100

    Added mult. and add. elements in some monoid

--- src/main/scala/algebra/Monoids.scala
+++ src/main/scala/algebra/Monoids.scala
@@ -25,3 +25,10 @@ trait MultiplicativeMonoid[T <: MonoidMultiplicative[T]] extends Monoid[T] with 
 }
 
 
+trait MonoidAdditive[that <: MonoidAdditive[that]] extends MonoidElement with MagmaAdditive[that] {
+    
+}
+
+trait MonoidMultiplicative[that <: MonoidMultiplicative[that]] extends MonoidElement with MagmaMultiplicative[that] {
+    
+}
