2d9982119aac32cd0b5849a077790b4237747c0e
Torstein Vik <torsteinv64@gmail.com>
2018-01-28 21:36:28 +0100

    Fixed Additive & Multiplicative Monoid signature, as new magma analogs

--- src/main/scala/algebra/Monoids.scala
+++ src/main/scala/algebra/Monoids.scala
@@ -13,15 +13,17 @@ trait Monoid[T <: MonoidElement] extends Magma[T]{
 }
 
 /** A [[Monoid]] where the elements are [[Additive]]. This allows [[combine]] to be inferred. */
-trait AdditiveMonoid[T <: MonoidElement with Additive[T]] extends Monoid[T] {
+trait AdditiveMonoid[T <: MonoidAdditive[T]] extends Monoid[T] with AdditiveMagma[T] {
     /** Synonym for the monoid identity for additive structures */
     def zero : T = identity
     override def combine (x : T, y : T) : T = x + y
 }
 
 /** A [[Monoid]] where the elements are [[Multiplicative]]. This allows [[combine]] to be inferred. */
-trait MultiplicativeMonoid[T <: MonoidElement with Multiplicative[T]] extends Monoid[T] {
+trait MultiplicativeMonoid[T <: MonoidMultiplicative[T]] extends Monoid[T] with MultiplicativeMagma[T] {
     /** Synonym for the monoid identity for multiplicative structures */
     def one : T = identity
     override def combine (x : T, y : T) : T = x * y
 }
+
+
