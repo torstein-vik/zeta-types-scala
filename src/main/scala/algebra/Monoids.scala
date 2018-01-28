7119737dbe585bbf658ad60860e250c830687e83
Torstein Vik <torsteinv64@gmail.com>
2018-01-28 21:37:10 +0100

    Removed monoid override combine, now supplied by magma

--- src/main/scala/algebra/Monoids.scala
+++ src/main/scala/algebra/Monoids.scala
@@ -16,14 +16,12 @@ trait Monoid[T <: MonoidElement] extends Magma[T]{
 trait AdditiveMonoid[T <: MonoidAdditive[T]] extends Monoid[T] with AdditiveMagma[T] {
     /** Synonym for the monoid identity for additive structures */
     def zero : T = identity
-    override def combine (x : T, y : T) : T = x + y
 }
 
 /** A [[Monoid]] where the elements are [[MonoidMultiplicative]]. This requries the identity "one" be declared */
 trait MultiplicativeMonoid[T <: MonoidMultiplicative[T]] extends Monoid[T] with MultiplicativeMagma[T] {
     /** Synonym for the monoid identity for multiplicative structures */
     def one : T = identity
-    override def combine (x : T, y : T) : T = x * y
 }
 
 
