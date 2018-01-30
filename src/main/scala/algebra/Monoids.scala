328af3a621078bffee08497ae64268d77b874a72
Torstein Vik <torsteinv64@gmail.com>
2018-01-30 23:27:50 +0100

    Made mon.rep.alg. immutable and elegant

--- src/main/scala/algebra/Monoids.scala
+++ src/main/scala/algebra/Monoids.scala
@@ -73,23 +73,18 @@ trait MonoidMultiplicative[that <: MonoidMultiplicative[that]] extends MonoidEle
 
 private object MonoidRepetitionAlgorithm {
 
-    // TODO: remove mutability
     def apply[T](f : (T, T) => T, n : Int, unit : T, x : T) : T = n match {
-        case _ if n >= 0 => {
-            var base : T = x
-            var exp = n
-            var result = unit
-            
-            while(exp > 0){
-                if((exp & 1) == 1){
-                    result = f(result, base)
-                }
-                base = f(base, base)
-                exp >>= 1
-            }
-            
-            return result
-        }
+        case 0 => unit
+        case 1 => x
+        case _ if n > 1 => applyImpl[T](f, n, unit, x) 
         case _ => throw new AlgebraicException("Monoidal repeated combination requires n >= 0")
+    }
+    
+    import scala.annotation.tailrec
+        
+    @tailrec
+    private def applyImpl[T](f : (T, T) => T, n : Int, acc : T, x : T) : T = n match {
+        case 0 => acc
+        case _ => applyImpl[T](f, n >> 1, if ((n & 1) == 1) f(acc, x) else acc, f(x, x))
     }
 }
