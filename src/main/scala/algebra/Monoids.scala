c28172c9bd0b16e02dc6231308c2cabae2d5e486
Torstein Vik <torsteinv64@gmail.com>
2018-01-30 22:44:50 +0100

    Removed Magma from Monoid trait signatures

--- src/main/scala/algebra/Monoids.scala
+++ src/main/scala/algebra/Monoids.scala
@@ -7,19 +7,19 @@ trait MonoidElement extends AlgebraicElement
 /** A [[Magma]] together with an [[identity]] 
  *  @tparam T The type of MonoidElement which the identity is of, and of the underlying Magma
  */
-trait Monoid[T <: MonoidElement] extends Magma[T]{
+trait Monoid[T <: MonoidElement] extends AlgebraicStructure[T] {
     /** The identity of this [[Monoid]] */
     def identity : T
 }
 
 /** A [[Monoid]] where the elements are [[MonoidAdditive]]. Provides synonym for the identity. */
-trait AdditiveMonoid[T <: MonoidAdditive[T]] extends Monoid[T] with AdditiveMagma[T] {
+trait AdditiveMonoid[T <: MonoidAdditive[T]] extends Monoid[T] {
     /** Synonym for the monoid identity for additive structures */
     def zero : T = identity
 }
 
 /** A [[Monoid]] where the elements are [[MonoidMultiplicative]]. Provides synonym for the identity. */
-trait MultiplicativeMonoid[T <: MonoidMultiplicative[T]] extends Monoid[T] with MultiplicativeMagma[T] {
+trait MultiplicativeMonoid[T <: MonoidMultiplicative[T]] extends Monoid[T] {
     /** Synonym for the monoid identity for multiplicative structures */
     def one : T = identity
 }
@@ -28,7 +28,7 @@ trait MultiplicativeMonoid[T <: MonoidMultiplicative[T]] extends Monoid[T] with 
 /** A [[MonoidElement]] with an addition defined on it
  *  @tparam that The type that this element may combined with and yield. that must be a subtype of MonoidAdditive[that]
  */
-trait MonoidAdditive[that <: MonoidAdditive[that]] extends MonoidElement with MagmaAdditive[that] {
+trait MonoidAdditive[that <: MonoidAdditive[that]] extends MonoidElement {
     /** The additive identity */
     val zero : that
     
@@ -42,7 +42,7 @@ trait MonoidAdditive[that <: MonoidAdditive[that]] extends MonoidElement with Ma
 /** A [[MonoidElement]] with a multiplication defined on it
  *  @tparam that The type that this element may combined with and yield. that must be a subtype of MonoidMultiplicative[that]
  */
-trait MonoidMultiplicative[that <: MonoidMultiplicative[that]] extends MonoidElement with MagmaMultiplicative[that] {
+trait MonoidMultiplicative[that <: MonoidMultiplicative[that]] extends MonoidElement {
     /** The multiplicative identity */
     val one : that
     
