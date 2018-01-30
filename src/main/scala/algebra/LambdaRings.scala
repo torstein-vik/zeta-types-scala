db40f29f0a80c33bea2d6c118da166aee7f63960
Torstein Vik <torsteinv64@gmail.com>
2018-01-30 23:58:53 +0100

    Greatly simplified signatures of LambdaRing by STDLambdaRing req. Qmult.

--- src/main/scala/algebra/LambdaRings.scala
+++ src/main/scala/algebra/LambdaRings.scala
@@ -11,42 +11,30 @@ trait LambdaRingElement[T <: LambdaRingElement[T]] extends RingElement[T]
 trait LambdaRing[E <: LambdaRingElement[E]] extends Ring[E] {
     /** The n-th Adams/psi operation applied to some element x */
     def psi (x : E)(n : Int) : E
-    /** The n-th lambda operation applied to some element x.
-     *  Also takes some implicit evidence that E can be converted to and from some other type S, which is a PartialQAlgebraElement
-     *  @tparam S the PartialQAlgebraElement the elements of this lambda ring visit in order to divide by integers
-     *  @param x the [[LambdaRingElement]] that has the lambda opertion applied
-     *  @param n specifies which n-th lambda operation to use
-     *  @param ev evidence that allows [[LambdaRingElement]] to convert into a [[PartialQAlgebraElement]], allowing division by integers
-     *  @param ev2 evidence that allows [[PartialQAlgebraElement]] to be re-embedded in this [[LambdaRing]], after division
-     */
-    def lambda[S <: PartialQAlgebraElement[S]] (x : E)(n : Int)(implicit ev: E => S, ev2: S => E) : E
+    /** The n-th lambda operation applied to some element x */
+    def lambda (x : E)(n : Int) : E
 }
 
 /** Standard implementation of [[LambdaRingElement]] */
-trait STDLambdaRingElement[T <: STDLambdaRingElement[T]] extends LambdaRingElement[T] {
+trait STDLambdaRingElement[T <: STDLambdaRingElement[T]] extends LambdaRingElement[T] with PartialQAlgebraElement[T] {
     /** The n-th Adams/psi operation applied to this element */
     def psi (n : Int) : T
     
     private var lambdacache : Map[Int, T] = Map()
-    /** The n-th lambda operation applied to this element.
-     *  Also takes some implicit evidence that E can be converted to and from some other type S, which is a PartialQAlgebraElement
-     *  @tparam S the PartialQAlgebraElement the elements of this lambda ring visit in order to divide by integers
-     *  @param n specifies which n-th lambda operation to use
-     *  @param ev evidence that allows this to be converted into a [[PartialQAlgebraElement]], allowing division by integers
-     *  @param ev2 evidence that allows [[PartialQAlgebraElement]] to be re-embedded into this type, after division
-     */
-    def lambda[S <: PartialQAlgebraElement[S]] (n : Int)(implicit ev: T => S, ev2: S => T) : T = n match {
+    
+    /** The n-th lambda operation applied to this element */
+    def lambda (n : Int) : T = n match {
         case 0 => one
         case 1 => this * one
         case n if lambdacache.contains(n) => lambdacache(n)
         case n if n > 1 => {
             var res = zero
             for (i <- 0 to n - 1) {
-                val ires = this.psi(n - i) * this.lambda(i)(ev, ev2)
+                val ires = this.psi(n - i) * this.lambda(i)
                 res += (if (i % 2 == 0) ires else ires.negation)
             }
             val coeff = Rational(if (n % 2 == 0) Integer(-1) else Integer(1), Integer(n))
-            ev(res).partialQMult(coeff).map(ev2) match {
+            res.partialQMult(coeff) match {
                 case None => throw new AlgebraicException("Wilkersons hypothesis did not hold! You may not use lambda in this ring")
                 case Some(r) => {
                     lambdacache += n -> r
@@ -59,7 +47,7 @@ trait STDLambdaRingElement[T <: STDLambdaRingElement[T]] extends LambdaRingEleme
 }
 
 /** Standard implementation of [[LambdaRing]], using [[STDLambdaRingElement]] */
-trait STDLambdaRing[E <: STDLambdaRingElement[E]] extends LambdaRing[E]{
+trait STDLambdaRing[E <: STDLambdaRingElement[E]] extends LambdaRing[E] with PartialQAlgebra[E] {
     override def psi (x : E)(n : Int) : E = x.psi(n)
-    override def lambda[S <: PartialQAlgebraElement[S]] (x : E)(n : Int)(implicit ev: E => S, ev2: S => E) : E = x.lambda(n)(ev, ev2)
+    override def lambda (x : E)(n : Int) : E = x.lambda(n)
 }
