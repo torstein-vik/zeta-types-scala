a0d7923b4cf0ad08f0eaa983c116cb7ec068833a
Torstein Vik <torsteinv64@gmail.com>
2018-02-02 18:25:24 +0100

    Implemented negation using mmap

--- src/main/scala/tannakiansymbols/TannakianSymbols.scala
+++ src/main/scala/tannakiansymbols/TannakianSymbols.scala
@@ -33,7 +33,7 @@ class TannakianSymbol[E <: MonoidElement] (val elements : Seq[(E, BigInt)])(impl
     
     override def *(that : TannakianSymbol[E]) = for (x <- this; y <- that) yield monoid.combine(x, y)
     
-    override def negation = new TannakianSymbol(elements.map({case (x, i) => (x, -i)}))
+    override def negation = mmap(-_)
     
     override def psi(n : Int) = new TannakianSymbol(elements.map({case (x, i) => (monoid.repeated(x, n), i)})).cleanup
     
