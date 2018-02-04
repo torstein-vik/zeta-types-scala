4cb926b17896d8282252cb7617d2316e4f1cda87
Torstein Vik <torsteinv64@gmail.com>
2018-02-05 00:21:35 +0100

    Removed aux. packages

--- src/main/scala/algebra/morphisms/package.scala
+++
@@ -1,7 +0,0 @@
-package io.github.torsteinvik.zetatypes.algebra
-
-/** Provides traits for various types of morphisms between algebraic structures.
- *  Also provides composition and identity isomorphisms, and some structure morphisms.
- *  @author Torstein Vik
- */
-package object morphisms 
