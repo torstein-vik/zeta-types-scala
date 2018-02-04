a8f26ef2590d99dba197da067a5b26af39f3ad16
Torstein Vik <torsteinv64@gmail.com>
2018-02-04 16:02:59 +0100

    Moved AlgebraicException into separate file

--- src/main/scala/algebra/Algebraic.scala
+++ src/main/scala/algebra/Algebraic.scala
@@ -5,9 +5,3 @@ trait AlgebraicElement
 
 /** Some structure of [[AlgebraicElement]] */
 trait AlgebraicStructure[T <: AlgebraicElement]
-
-/** An exception coming from some algebraic method 
- *  @constructor Create an algebraic exception
- *  @param msg The message to be displayed alongside the exception
- */
-class AlgebraicException(msg : String) extends Exception(msg)
---
+++ src/main/scala/algebra/AlgebraicException.scala
@@ -0,0 +1,7 @@
+package io.github.torsteinvik.zetatypes.algebra
+
+/** An exception coming from some algebraic method 
+ *  @constructor Create an algebraic exception
+ *  @param msg The message to be displayed alongside the exception
+ */
+class AlgebraicException(msg : String) extends Exception(msg)
