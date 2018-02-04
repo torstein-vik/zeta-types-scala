3e0ec7d79b78f4e8697e552724ef6df8d0d5840f
Torstein Vik <torsteinv64@gmail.com>
2018-02-04 16:03:39 +0100

    Removed AlgebraicStructure

--- src/main/scala/algebra/Algebraic.scala
+++ src/main/scala/algebra/Algebraic.scala
@@ -2,6 +2,3 @@ package io.github.torsteinvik.zetatypes.algebra
 
 /** An element of some [[AlgebraicStructure]] */
 trait AlgebraicElement
-
-/** Some structure of [[AlgebraicElement]] */
-trait AlgebraicStructure[T <: AlgebraicElement]
