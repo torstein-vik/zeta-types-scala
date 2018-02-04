57db0e0dbac80ed611ad2b0c2757acc48ea4bf08
Torstein Vik <torsteinv64@gmail.com>
2018-02-04 15:50:20 +0100

    Removed all auxiliary packages

--- src/main/scala/algebra/morphisms/Composition.scala
+++
@@ -1,9 +0,0 @@
-package io.github.torsteinvik.zetatypes.algebra.morphisms
-
-import io.github.torsteinvik.zetatypes.algebra._
-
-private[morphisms] class Composition[A <: AlgebraicElement, B <: AlgebraicElement, C <: AlgebraicElement]
-    (f : Homomorphism[B, C], g : Homomorphism[A, B]) extends Homomorphism[A, C] {
-    def apply (x : A) = f(g(x))
-}
-
--- src/main/scala/tannakiansymbols/package.scala
+++
@@ -1,9 +0,0 @@
-package io.github.torsteinvik.zetatypes
-
-/** Provides classes for working with Tannakian Symbols, and some tools for 
- *  doing this with a nice syntax. Most importantly, the [[TannakianSymbol]]
- *  class implements all computational behaviour. AlgebraicMultisets are used to
- *  to instaniate Tannakian Symbols.
- *  @author Torstein Vik
- */
-package object tannakiansymbols {}
