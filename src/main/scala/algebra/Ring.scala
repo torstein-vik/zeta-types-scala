d4a811e51b743ac85fc5a7a5337da924d9d34fa6
Torstein Vik <torsteinv64@gmail.com>
2018-02-05 23:20:43 +0100

    Added PartialQAlgebra and QAlgebra aggregations

--- src/main/scala/algebra/Ring.scala
+++ src/main/scala/algebra/Ring.scala
@@ -1,3 +1,5 @@
 package io.github.torsteinvik.zetatypes.algebra
 
 trait Ring[T] extends AdditiveGroup[T] with MultiplicativeMonoid[T]
+trait PartialQAlgebra[T] extends Ring[T] with PartialQModuleDivisionProvider[T, T] 
+trait QAlgebra[T] extends PartialQAlgebra[T] with QModuleDivisionProvider[T, T] 
