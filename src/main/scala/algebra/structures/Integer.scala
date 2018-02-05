7779a30b6a82cef003547424df5bbd7d2ab7dce8
Torstein Vik <torsteinv64@gmail.com>
2018-02-05 22:46:50 +0100

    Removed old Integer algebraic systemization

--- src/main/scala/algebra/structures/Integer.scala
+++ src/main/scala/algebra/structures/Integer.scala
@@ -7,27 +7,6 @@ import io.github.torsteinvik.zetatypes.algebra._
  *  @param x the number that this Integer is created from
  */
 case class Integer (x : BigInt) extends PsiRingElement[Integer] with PartialQModuleElement[Integer]{
-    
-    override def +(that : Integer) = Integer(x + that.x) 
-    override def *(that : Integer) = Integer(x * that.x)
-    override def negation = Integer(-x)
-    
-    override def psi(n : Int) = this
-    
-    override def partialQMult(n : Rational) : Option[Integer] = {
-        val Rational(Integer(num), Integer(den)) = n
-        val (div, mod) = (num * x) /% den
-        if (mod == 0) {
-            return Some(Integer(div))
-        } else {
-            return None
-        }
-    }
-    
     override def toString() = x.toString()
-    
-    override lazy val canonicalRing = Integers
 }
 
-/** The ring of [[Integer]]s */
-object Integers extends RingClass[Integer](Integer(0), Integer(1)) with PsiRing[Integer] with PartialQAlgebra[Integer] 
