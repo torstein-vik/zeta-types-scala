9f4bc662870bd289978f904618afbfc70dbc5fcd
Torstein Vik <torsteinv64@gmail.com>
2018-02-05 23:02:26 +0100

    Added negation and inverse element provider-bindings

--- src/main/scala/algebra/Element.scala
+++ src/main/scala/algebra/Element.scala
@@ -6,6 +6,9 @@ trait Element {
     def +[T, S](y : T)(implicit provider : AdditionProvider       [this.type, T, S]) : S = provider.add(this, y)
     def *[T, S](y : T)(implicit provider : MultiplicationProvider [this.type, T, S]) : S = provider.multiply(this, y)
     
+    def negation [T](implicit provider : NegationProvider [this.type, T]) : T = provider.negate(this)
+    def inverse  [T](implicit provider : InverseProvider  [this.type, T]) : T = provider.invert(this)
+    
 }
 
 object Element {
