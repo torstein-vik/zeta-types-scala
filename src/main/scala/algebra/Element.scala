14751e4005747380d2d0284d31eacb018f827001
Torstein Vik <torsteinv64@gmail.com>
2018-02-05 23:05:59 +0100

    Added subtraction and division Element provider-bindings

--- src/main/scala/algebra/Element.scala
+++ src/main/scala/algebra/Element.scala
@@ -6,6 +6,8 @@ trait Element {
     def +[T, S](y : T)(implicit provider : AdditionProvider       [this.type, T, S]) : S = provider.add(this, y)
     def *[T, S](y : T)(implicit provider : MultiplicationProvider [this.type, T, S]) : S = provider.multiply(this, y)
     
+    def -[T, S](y : T)(implicit provider : SubtractionProvider [this.type, T, S]) : S = provider.subtract(this, y)
+    def /[T, S](y : T)(implicit provider : DivisionProvider    [this.type, T, S]) : S = provider.divide(this, y)
     
     def unary_-  [T](implicit provider : NegationProvider [this.type, T]) : T = provider.negate(this)
     def unary_~  [T](implicit provider : InverseProvider  [this.type, T]) : T = provider.invert(this)
