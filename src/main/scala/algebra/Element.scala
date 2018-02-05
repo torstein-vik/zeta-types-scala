925ed69466bfedc4a2f58326f12ef6489369d480
Torstein Vik <torsteinv64@gmail.com>
2018-02-05 22:59:35 +0100

    Added Element provider-binding for addition and multiplication

--- src/main/scala/algebra/Element.scala
+++ src/main/scala/algebra/Element.scala
@@ -3,6 +3,9 @@ package io.github.torsteinvik.zetatypes.algebra
 /** An element in the algebraic system */
 // Macros would be so much better!
 trait Element {
+    def +[T, S](y : T)(implicit provider : AdditionProvider       [this.type, T, S]) : S = provider.add(this, y)
+    def *[T, S](y : T)(implicit provider : MultiplicationProvider [this.type, T, S]) : S = provider.multiply(this, y)
+    
 }
 
 object Element {
