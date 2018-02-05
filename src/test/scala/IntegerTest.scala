e33fdf50311bc0c62f3b3cb5a9ca3370b47a5796
Torstein Vik <torsteinv64@gmail.com>
2018-02-05 01:30:11 +0100

    Removed duplicate Integer tests

--- src/test/scala/IntegerTest.scala
+++ src/test/scala/IntegerTest.scala
@@ -28,29 +28,6 @@ class IntegerTest extends FunSuite {
         assert(Integer(b).toString === b.toString)
         assert(Integer(c).toString === c.toString)
     }
-        
-    test ("zero and one test") {
-        assert(Integers.zero === Integer(0))
-        assert(Integers.one  === Integer(1))
-    }
-    
-    test ("additive inverse test") {
-        assert(Integers.additive.invert(Integer(a)) === Integer(-a))
-        assert(Integers.additive.invert(Integer(b)) === Integer(-b))
-        assert(Integers.additive.invert(Integer(c)) === Integer(-c))
-    }
-    
-    test ("addition test") {
-        assert(Integers.additive.combine(Integer(a), Integer(b)) === Integer(a + b))
-        assert(Integers.additive.combine(Integer(b), Integer(c)) === Integer(b + c))
-        assert(Integers.additive.combine(Integer(c), Integer(a)) === Integer(c + a))
-    }
-    
-    test ("multiplication test") {
-        assert(Integers.multiplicative.combine(Integer(a), Integer(b)) === Integer(a * b))
-        assert(Integers.multiplicative.combine(Integer(b), Integer(c)) === Integer(b * c))
-        assert(Integers.multiplicative.combine(Integer(c), Integer(a)) === Integer(c * a))
-    }
     
     test ("notation adidition test") {
         assert(Integer(a) + Integer(b) === Integer(a + b))
