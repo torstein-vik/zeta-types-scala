deb7e10bdad6d5c8291a0606c174662214116059
Torstein Vik <torsteinv64@gmail.com>
2018-02-05 23:25:23 +0100

    Added PsiRings, as lambda-rings where lambda is inferred from psi (code based on master branch)

--- src/main/scala/algebra/LambdaRing.scala
+++ src/main/scala/algebra/LambdaRing.scala
@@ -10,3 +10,24 @@ trait LambdaProvider[-X, +T] {
 
 trait LambdaRing[T] extends Ring[T] with PsiProvider[T, T] with LambdaProvider[T, T]
 
+trait PsiRing[T] extends LambdaRing[T] with PartialQAlgebra[T] {
+    // Unpure optimization (memoization)
+    import collection.mutable.Map
+    
+    private[PsiRing] var lambdacache : Map[(T, Int), T] = Map()
+    
+    override final def lambda (x : T)(n : Int) = n match {
+        case 0 => one
+        case 1 => x
+        case n if lambdacache.contains((x, n)) => lambdacache((x, n))
+        case n if n > 1 => partialmdivide((0 to n - 1).foldLeft(zero) {
+            case (sum, i) if i % 2 == 0 =>      add(sum, multiply(psi(x)(n - i), lambda(x)(i)))
+            case (sum, i) if i % 2 == 1 => subtract(sum, multiply(psi(x)(n - i), lambda(x)(i)))
+        })(if (n % 2 == 0) -n else n) match {
+            case Some(result) => {lambdacache += (x, n) -> result; result}
+            case None => throw new AlgebraicException("Wilkersons hypothesis did not hold! You may not use lambda in this ring")
+        }
+        case _ => throw new AlgebraicException("Lambda operation only defined for n >= 0 !")
+    }
+}
+
