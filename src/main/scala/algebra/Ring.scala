cfdba0580d90164357f9c4a21c5eb359fe197454
Torstein Vik <torsteinv64@gmail.com>
2018-02-07 01:29:35 +0100

    Added Field, extending and implementing mdivide

--- src/main/scala/algebra/Ring.scala
+++ src/main/scala/algebra/Ring.scala
@@ -3,3 +3,7 @@ package io.github.torsteinvik.zetatypes.algebra
 trait Ring[T] extends AdditiveGroup[T] with MultiplicativeMonoid[T]
 trait PartialQAlgebra[T] extends Ring[T] with PartialQModuleDivisionProvider[T, T] 
 trait QAlgebra[T] extends PartialQAlgebra[T] with QModuleDivisionProvider[T, T] 
+
+trait Field[T] extends Ring[T] with MultiplicativeGroup[T] with QAlgebra[T] {
+    def mdivide (x : T)(n : Int) = divide(x, mmultiply(one)(n))
+}
