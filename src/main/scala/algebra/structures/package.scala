716be82ebb91de781858d559fff21fe68a90b248
Torstein Vik <torsteinv64@gmail.com>
2018-02-07 01:39:12 +0100

    Added Rational type synonym to structures package

--- src/main/scala/algebra/structures/package.scala
+++ src/main/scala/algebra/structures/package.scala
@@ -3,4 +3,7 @@ package io.github.torsteinvik.zetatypes.algebra
 /** Provides instances of various algerbraic structures.
  *  @author Torstein Vik
  */
-package object structures {}
+package object structures {
+    /** Type synonym for rational number */
+    type Rational = Fraction[Integer]
+}
